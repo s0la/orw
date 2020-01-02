619d0082869aca73c685447a9b4e4e8f7c9ec57e
sola <sola931@gmail.com>
2020-01-02 01:12:02 +0100

    Add edit bar option support

--- scripts/barctl.sh
+++ scripts/barctl.sh
@@ -2,6 +2,8 @@
 
 get_bars() {
 	bars=( $(ps aux | awk '! /awk/ && /lemonbar/ { print $NF }') )
+	bar_count=${#bars[*]}
+
 }
 
 kill_bar() {
@@ -38,7 +40,7 @@ start_bar_on_boot() {
 
 initial_ram_usage=$(${0%/*}/check_memory_consumption.sh Xorg)
 
-while getopts :ds:c:gb:r:kla flag; do
+while getopts :ds:c:gb:r:E:e:kla flag; do
 	case $flag in
 		g)
 			bar=$(sed "s/.*-n \(\w*\).*/\1/" <<< $@)
@@ -49,8 +51,59 @@ while getopts :ds:c:gb:r:kla flag; do
 			~/.orw/scripts/bar/generate_bar.sh ${@:2}
 			exit;;
 		c) check_interval=$OPTARG;;
-		b) bars=( ${OPTARG//,/ } );;
+		b)
+			bars=( ${OPTARG//,/ } )
+			bar_count=${#bars[*]};;
 		r) ram_tolerance=$OPTARG;;
+		E) inherit_config=~/.config/orw/bar/configs/$OPTARG;;
+		e)
+			all="$@"
+			replace="${all#*-e }"
+
+			[[ $inherit_config ]] &&
+				args=$(sed -n "s/.*\($replace[^-]*\).*/\1/p" $inherit_config) || args="${replace#* }"
+
+			flag="${replace%% *}"
+
+			if [[ $args =~ [+-][0-9] ]]; then
+				pre="${args%%[+-][0-9]*}"
+				post="${args#$pre}"
+
+				relative=${post%% *}
+				sign=${relative:0:1}
+				value=${relative:1}
+
+				spaces="${pre//[^ ]/}"
+				index="${#spaces}"
+			fi
+
+			[[ $first_arg =~ [+-][0-9] ]] && sign=${first_arg}
+
+			((bar_count)) || get_bars
+			((bar_count > 1)) && all_bars="${bars[*]}" || bar=$bars
+
+			replace_config=$(eval echo ~/.config/orw/bar/configs/${bar:-{${all_bars// /,}\}})
+
+			awk -i inplace '\
+				BEGIN {
+					f = "'$flag'"
+					v = '${value:-0}'
+					i = '${index:-0}'
+				} {
+					if("'$sign'") {
+						cv = gensub(".*" f "( [^ ]*){" i + 1 "}.*", "\\1", 1)
+						nv = cv '$sign' v
+
+						p = "'"$pre"'" nv " '"${post/$relative/}"'"
+					} else {
+						p = "'"$args"' "
+					}
+
+					gsub(f "[^-]*", f " " p)
+				} {
+					print
+				}' $replace_config
+			break;;
 		k)
 			if [[ $bars ]]; then
 				for bar in "${bars[@]}"; do
