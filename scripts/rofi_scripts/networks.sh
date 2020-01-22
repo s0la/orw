7d01daf0450c842830b07cf71867b680dcab8e55
s0la <sola931@gmail.com>
2020-01-22 05:04:37 +0000

    Add disconnect option and notifications

--- scripts/rofi_scripts/networks.sh
+++ scripts/rofi_scripts/networks.sh
@@ -31,23 +31,34 @@ if [[ -z $@ ]]; then
 		/padding/ && NR < nr + 5 { p = get_value() }
 		END { print int((('$display_width' / 100) * w - 2 * p) / (8 - 2) - 7) }' .config/rofi/list.rasi)
 
-	nmcli dev wifi | awk '{ \
-		if(NR == 1) {
-			si = index($0, "SSID")
-			mi = index($0, "MODE")
-		} else {
+		nmcli dev wifi | awk '{ \
+			if(NR == 1) {
+				si = index($0, " SSID")
+				mi = index($0, " MODE")
+			} else {
 			nn = substr($0, si, (mi - si) - 1)
 			o = '$offset' - length(nn)
 			printf "%s %s %*s\n", $NF != "--" ? " " : " ", nn, o, $1 == "*" ? "connected" : ""
 		}
-	}'
+}'
 else
 	get_password() {
 		password=$(sed -n "s/^$network_name: //p" ~/.orw/scripts/auth/networks)
 	}
 
+	notify_on_finish() {
+		while kill -0 $pid 2> /dev/null; do
+			sleep 1
+		done && ~/.orw/scripts/notify.sh "Successfully ${state-connected to} to <b>$network_name</b>"
+	}
+
 	read protected network_name <<< $@
-	[[ $network_name =~ connected$ ]] && network_name="${network_name% *}"
+
+	if [[ $network_name =~ connected$ ]]; then
+		network_name="${network_name% *}"
+		state='disconnected from'
+		connected=true
+	fi
 
 	if [[ $protected ==  ]]; then
 		get_password
@@ -58,8 +69,13 @@ else
 			get_password
 		fi
 
-		password="password $password"
+		password_arg=password
 	fi
 
-	nmcli device wifi connect "$network_name" "$password"
+	[[ $connected ]] && command="nmcli connection down '$network_name'" ||
+		command="nmcli device wifi connect '$network_name' $password_arg '$password'"
+
+	coproc (eval $command &> /dev/null &)
+	pid=$((COPROC_PID + 1))
+	coproc (notify_on_finish &)
 fi
