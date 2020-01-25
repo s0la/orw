0a1af26ed681a4c6e97d10dc029108ddaf7226dc
s0la <sola931@gmail.com>
2020-01-25 02:00:03 +0000

    Small syntax improvments

--- scripts/rofi_scripts/mount_dmenu.sh
+++ scripts/rofi_scripts/mount_dmenu.sh
@@ -2,8 +2,13 @@
 
 rofi='rofi -dmenu -theme main'
 
-[[ $@ ]] && dev=$1 || dev=$(lsblk -lpo +model | awk '{ if($1 ~ /sd.$/ && $7) { model=""; for(f=7; f<=NF; f++) model=model$f" " };\
-	if($6 == "part" && $4 ~ /[0-9]G/ && $7 !~ /^\//) printf("%-45s %-20s %s\n", model, $4, $1)}' | $rofi)
+[[ $@ ]] && dev=$1 ||
+	dev=$(lsblk -lpo +model | awk '{
+		if($1 ~ /sd.$/ && $7) {
+			m=""
+			for(f = 7; f <= NF; f++) m = m $f " "
+		}
+		if($6 == "part" && $4 ~ /[0-9]G/ && $7 !~ /^\//) printf("%-45s %-20s %s\n", m, $4, $1)}' | $rofi)
 
 mount() {
 	~/.orw/scripts/mount.sh ${dev##* } "${dev% *}" "$mount_point"
@@ -11,9 +16,17 @@ mount() {
 
 if ! $(mount); then
 	mount_point=$(find /mnt -maxdepth 1 -type d | $rofi)
+
 	if [[ ! -d $mount_point ]]; then
-		confirmation=$( echo -e 'yes\nno' | rofi -dmenu -p "Mount point doesn't exist, would you like to create it?")
-		[[ $confirmation == yes ]] && sudo mkdir $mount_point || exit
+		confirmation=$(echo -e 'yes\nno' |\
+			rofi -dmenu -p "Mount point doesn't exist, would you like to create it?")
+
+		if [[ $confirmation == yes ]]; then
+			[[ $mount_point =~ ^/home ]] || sudo=sudo
+			mkdir $sudo $mount_point
+		else
+			exit
+		fi
 	fi
 
 	mount
