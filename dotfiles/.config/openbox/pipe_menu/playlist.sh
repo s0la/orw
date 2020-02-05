1b699a21ca9c6a37045688dc691ddf92f9a5baf7
s0la <sola931@gmail.com>
2020-02-05 02:45:00 +0000

    Add current song indicator

--- dotfiles/.config/openbox/pipe_menu/playlist.sh
+++ dotfiles/.config/openbox/pipe_menu/playlist.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+indicator='●'
+indicator=''
+
+current_song="$(mpc current -f "%artist% - %title%")"
+[[ $current_song ]] && empty='____'
+
 while read -r song; do
-	songs+=( "${song// /_}:\"$song\"" )
-done <<< $(mpc playlist)
+	songs+=( "${song//&/&amp;}" )
+	#songs+=( "${song// /_}:\"$song\"" )
+	#songs+=( "${song//\&/\\&}:\"$song\"" )
+done <<< "$( mpc playlist |\
+	awk '{ p = ($0 == "'"$current_song"'") ? "'$indicator'" : "'"$empty"'"
+	print p "__" gensub(" ", "_", "g") ":\"" $0 "\"" }')"
 
 ~/.config/openbox/pipe_menu/generate_menu.sh -c '~/.orw/scripts/play_song_from_playlist.sh' -i "${songs[@]}"
