8eeb88d1d22e406ab914cd869137b18b71a99a30
s0la <sola931@gmail.com>
2020-02-05 02:19:56 +0000

    Add guide if flags are not provided

--- scripts/add_bar_launcher.sh
+++ scripts/add_bar_launcher.sh
@@ -7,28 +7,40 @@ while getopts :i:n:c:r:m:u:d: flag; do
 		i) icon=$OPTARG;;
 		n) name="$OPTARG";;
 		c) left="$OPTARG";;
-		r) right="\"$OPTARG\"";;
-		m) middle="\"$OPTARG\"";;
-		u) up="\"$OPTARG\"";;
-		d) down="\"$OPTARG\"";;
+		r) right="$OPTARG";;
+		m) middle="$OPTARG";;
+		u) up="$OPTARG";;
+		d) down="$OPTARG";;
 	esac
 done
+
+if [[ -z $@ ]]; then
+	read -p 'Name: ' name
+	read -p 'Icon: ' icon
+	read -p 'Left click command: ' left
+	read -p 'Right click command: ' right
+	read -p 'Middle click command: ' middle
+	read -p 'Scroll up command: ' up
+	read -p 'Scroll down command: ' down
+fi
 
 [[ $name ]] || name=$(xwininfo | awk -F ' - |"' '/id:/ { print $(NF - 1) }')
 
 offset=$(awk '/^#?icon/ { print gensub(/.*%{I(.[0-9]+).*/, "\\1", 1); exit }' $launchers)
 
-cat <<- EOF >> $launchers
+lines=$(wc -l < $launchers)
+((lines)) && echo >> $launchers
 
+cat <<- EOF >> $launchers
 	#$name
 	icon="%{I$offset}${icon// /}%{I-}"
 	name="$name"
 	left="$left"
 EOF
 
-[[ $right ]] && echo "right=$right" >> $launchers
-[[ $middle ]] && echo "middle=$middle" >> $launchers
-[[ $up ]] && echo "up=$up" >> $launchers
-[[ $down ]] && echo "down=$down" >> $launchers
+[[ $right ]] && echo "right=\"$right\"" >> $launchers
+[[ $middle ]] && echo "middle=\"$middle\"" >> $launchers
+[[ $up ]] && echo "up=\"$up\"" >> $launchers
+[[ $down ]] && echo "down=\"$down\"" >> $launchers
 
 exit 0
