645fe9a1f205405f6c94f366d5acefcca34b1897
s0la <sola931@gmail.com>
2020-02-05 02:27:32 +0000

    Remove comments

--- scripts/rofi_scripts/apps.sh
+++ scripts/rofi_scripts/apps.sh
@@ -4,18 +4,6 @@ if [[ -z $@ ]]; then
 	indicator=''
 	indicator='●'
 
-	#eval $(wmctrl -l | awk '\
-	#	{
-	#		a = $NF
-	#		if(a == "vifm") r = r " vifm=\"'$indicator' \""
-	#		else if(a == "termite") r = r " termite=\"'$indicator' \""
-	#		else if(a == "DROPDOWN") r = r " dropdown=\"'$indicator' \""
-	#		else if(a == "qutebrowser") r = r " qutebrowser=\"'$indicator' \""
-	#	} END { print r }')
-
-	##echo -e "lock\ntile\n${termite}termite\n${dropdown}dropdown\n${vifm}file manager\n${qutebrowser}web browser"
-	#echo -e "lock\ntile\n${vifm}vifm\n${termite}termite\n${dropdown}dropdown\n${qutebrowser}qutebrowser"
-
 	running="$(wmctrl -l | awk '\
 		{
 			a = $NF
@@ -27,8 +15,6 @@ if [[ -z $@ ]]; then
 
 	[[ $running ]] && eval "$running empty='  '"
 
-	#echo -e "lock\ntile\n${termite}termite\n${dropdown}dropdown\n${vifm}file manager\n${qutebrowser}web browser"
-	#echo -e "  lock\n  tile\n${vifm- } vifm\n${termite- } termite\n${dropdown- } dropdown\n${qutebrowser- } qutebrowser"
 	cat <<- EOF
 		${empty}lock
 		${empty}tile
@@ -44,12 +30,10 @@ else
         *dropdown*) ~/.orw/scripts/dropdown.sh ${@#*dropdown};;
 		*tile*) ~/.orw/scripts/tile_terminal.sh ${@#tile};;
         *termite*) termite -t termite ${@#*termite};;
-        #*file*) ~/.orw/scripts/vifm.sh ${@#*manager};;
-        #*web*) qutebrowser ${@#*browser};;
         *vifm*) ~/.orw/scripts/vifm.sh ${@#*vifm};;
-        *qutebrowser*) qutebrowser ${@#*browser};;
+        *qutebrowser*)
+			[[ ! $@ =~ private ]] && qutebrowser ${@#*browser} ||
+				qutebrowser -s content.private_browsing true;;
 		*lock) ~/.orw/scripts/lock_screen.sh;;
-        #web*) firefox ${@#*browser};;
-        #*file*) thunar ${@#*manager};;
     esac
 fi
