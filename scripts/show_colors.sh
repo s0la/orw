84abd85941dc013fc0f091d453bcf95e0c2deb34
s0la <sola931@gmail.com>
2020-02-05 02:19:03 +0000

    Add spacing

--- scripts/show_colors.sh
+++ scripts/show_colors.sh
@@ -10,44 +10,32 @@ while getopts :b:d:l:f: flag; do
 done
 
 colorctl=~/.orw/scripts/colorctl.sh
-colors=~/.config/orw/colorschemes/colors
+all_colors=~/.config/orw/colorschemes/colors
 
 clean="$(tput sgr0)"
 dark=$($colorctl -cs ';' -h ${hex_dark-'#404040'})
 bright=$($colorctl -cs ';' -h ${hex_bright-'#bbbbbb'})
 
-#while read index name value rgb br; do
-#	printf "%d %s\n\033[48;2;%s38;2;%s2m%*s%s%*s\033[0m\n" \
-#		$index $name $rgb ${!br} ${length:=5} ' ' ${value:1} $length ' '
-#done <<< $(awk -Wposix '\
-#			/'$filter'/ {
-#				r = substr($2,2,2)
-#				g = substr($2,4,2)
-#				b = substr($2,6,2)
-#				rgb = sprintf("%d %d %d", "0x" r, "0x" g, "0x" b)
-#				split(rgb, rgba)
-#				r = rgba[1]; g = rgba[2]; b = rgba[3]
-#				bi = (0.3 * r + 0.6 * g + 0.1 * b) / 255
-#				printf "%d %s %s %d;%d;%d; %s\n", NR, $1, $2, r, g, b, (bi > 0.5) ? "bright" : "dark" }' $colors)
+colors="$(awk -Wposix '\
+	BEGIN {
+		l = '${length:-30}'
+		br = "'$bright'"
+		dr = "'$dark'"
+	}
+	/'$filter'/ {
+		r = substr($2,2,2)
+		g = substr($2,4,2)
+		b = substr($2,6,2)
+		rgb = sprintf("%d;%d;%d;", "0x" r, "0x" g, "0x" b)
+		split(rgb, rgba, ";")
+		r = rgba[1]; g = rgba[2]; b = rgba[3]
 
-echo -e "$(awk -Wposix '\
-			BEGIN {
-				l = '${length:-30}'
-				br = "'$bright'"
-				dr = "'$dark'"
-			}
-			/'$filter'/ {
-				r = substr($2,2,2)
-				g = substr($2,4,2)
-				b = substr($2,6,2)
-				rgb = sprintf("%d;%d;%d;", "0x" r, "0x" g, "0x" b)
-				split(rgb, rgba, ";")
-				r = rgba[1]; g = rgba[2]; b = rgba[3]
+		bi = (0.3 * r + 0.6 * g + 0.1 * b) / 255
+		cl = length(NR) + length($1) + length($2)
+		p = sprintf("%*s", int((l - cl) / 2), " ")
+		e = (cl % 2 > 0) ? " " : ""
 
-				bi = (0.3 * r + 0.6 * g + 0.1 * b) / 255
-				cl = length(NR) + length($1) + length($2)
-				p = sprintf("%*s", int((l - cl) / 2), " ")
-				e = (cl % 2 > 0) ? " " : ""
+		printf "\033[48;2;%s38;2;%s2m%s %d %s %s %s%s\033[0m\n\n", \
+			rgb, (bi > 0.5) ? dr : br, p, NR, $1, $2, p, e }' $all_colors)"
 
-				printf "\033[48;2;%s38;2;%s2m%s %d %s %s %s%s\033[0m\n\n", \
-					rgb, (bi > 0.5) ? dr : br, p, NR, $1, $2, p, e }' $colors)"
+echo -e "\n$colors\n"
