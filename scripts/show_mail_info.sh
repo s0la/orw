478fc587dd4c1ab21db49f7c1d0ba37ee042e515
s0la <sola931@gmail.com>
2020-02-05 02:18:31 +0000

    Change format and add limit

--- scripts/show_mail_info.sh
+++ scripts/show_mail_info.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+count=$3
+
 while read -r mail_info; do
 	all_mail_info+="$mail_info\n\n"
 done <<< $(curl -u "$1":"$2" --silent "https://mail.google.com/mail/feed/atom" |
-	xmllint --format - 2> /dev/null | awk -F '[<>]' '/name|title/ && ! /Inbox/ \
-	{ if($2 == "title" && $3) t = " - " $3; else { print $3 t } }' )
+	xmllint --format - 2> /dev/null | awk -F '[<>]' '
+		/name|title/ && ! /Inbox/ {
+			if('$count' && c && c > '$count') exit
+			else if($2 == "title" && $3) {
+				c++
+				e = (length($3) > 80) ? ".." : ""
+				t = sprintf("\\n%.80s%s", $3, e)
+			} else print "<b>" $3 "</b>" t }' )
 
-~/.orw/scripts/notify.sh -f 9 -t 10 -p "$all_mail_info"
+~/.orw/scripts/notify.sh -f 8 -t 10 -p "$all_mail_info"
