6196ad832da055e2209f4de6c2b3255e20c00e75
s0la <sola931@gmail.com>
2020-02-11 15:19:30 +0000

    Move icon above bar

--- scripts/volume_notification.sh
+++ scripts/volume_notification.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
 
-icon=~/Downloads/audio-volume-medium-symbolic.svg
-
 color_bar() {
 	for p in $(seq $2); do
 		eval $1+='▀'
 	done
 }
 
-read level_value empty_value <<< $(amixer -D pulse get Master | awk -F '[[%]' \
-	'/^ *Front/ { s = 5; t = 100 / s; l = $2 / s; printf "%.0f %.0f", l, t - l; exit }')
+step=4
+icon_size=11
+distance=$(((100 / step) / 2))
+
+read level_value empty_value <<< $(amixer -D pulse get Master | \
+	awk -F '[[%]' '\
+		/^ *Front/ {
+			s = '$step'
+			t = int(100 / s)
+			l = int($2 / s)
+			printf "%.0f %.0f", l, t - l
+			exit
+		}')
 
 color_bar level $level_value
 color_bar empty $empty_value
 
-icon=
+icon="$(printf "%*.s%s\\\n%s" $distance " " \
+	"<span font='Roboto Mono $icon_size' foreground='\$epbfg'> </span>" \
+	"<span font='Roboto Mono $icon_size'> </span>")"
 
 volume_bar="<span font='Roboto 8' foreground='\$pbfg'>$level<span foreground='\$epbfg'>$empty</span></span>"
 
-~/.orw/scripts/notify.sh -r 100 -P 4 -p "<span font='Roboto 13' foreground='\$epbfg'>$icon  $volume_bar</span>"
+~/.orw/scripts/notify.sh -r 100 -P 4 -p "$icon$volume_bar"
