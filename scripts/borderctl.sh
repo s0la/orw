dc48f73cc575e700ed4755e700473af5833c2219
s0la <sola931@gmail.com>
2020-03-22 10:59:25 +0000

    Change client options naming

--- scripts/borderctl.sh
+++ scripts/borderctl.sh
@@ -158,9 +158,9 @@ case $1 in
 		case $1 in
 			hw) pattern=handle.width;;
 			bw) pattern=^border.width;;
-			cw) pattern='client.*.(width|height)';;
-			cph) pattern='client.padding.height';;
-			cpw) pattern='client.padding.width';;
+			cp) pattern='client.*padding';;
+			ch) pattern='client.padding.height';;
+			cw) pattern='client.padding.width';;
 			jt) pattern=label.*justify;;
 			pw) pattern=^padding.width;;
 			ph) pattern=^padding.height;;
