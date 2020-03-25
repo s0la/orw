d56f2314ade2761f5b8ef011fd422608849e1659
s0la <sola931@gmail.com>
2020-03-25 12:23:57 +0000

    Add wallpaper_category_selection

--- scripts/rofi_scripts/wallpaper_group.sh
+++ scripts/rofi_scripts/wallpaper_group.sh
@@ -4,5 +4,6 @@ path=~/.orw/scripts/rofi_scripts
 ${path%/*}/set_rofi_margins.sh
 
 modis+="wallpapers:$path/wallpapers.sh,"
+modis+="category_selection:$path/wallpaper_category_selection.sh,"
 modis+="wallpaper_categories:$path/wallpaper_categories.sh"
 rofi -modi "$modis" -show $1 -theme main
