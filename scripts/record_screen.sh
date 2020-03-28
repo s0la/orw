76fa0528f325682eac3efb84c0476592e53ca904
s0la <sola931@gmail.com>
2020-03-28 20:14:56 +0000

    Fix display position

--- scripts/record_screen.sh
+++ scripts/record_screen.sh
@@ -10,12 +10,16 @@ while getopts :f:d: flag; do
 done
 
 [[ ! -f ~/.config/orw/config ]] && ~/.orw/scripts/generate_orw_config.sh
-resolution=$(awk '/^'${display-full_resolution}' / { print $2 "x" $3 }' ~/.config/orw/config)
+
+read resolution position <<< $(awk '\
+	/^'${display-full_resolution}'/ {
+		if(/^full/ || xy) print $2 "x" $3, xy
+		else xy = "+" $2 "," $3 }' ~/.config/orw/config)
 
 shift $((arg_count * 2))
 [[ $@ ]] && filename="$@" || filename=$(date +"%Y-%m-%d-%H:%M")
 
 ~/.orw/scripts/notify.sh -p "<span font='Roboto Mono 10'>      </span>recording started"
 
-ffmpeg -y -f x11grab -r ${fps-25} -s $resolution -draw_mouse 0 -i $DISPLAY \
+ffmpeg -y -f x11grab -r ${fps-25} -s $resolution -draw_mouse 0 -i $DISPLAY$position \
 	-f pulse -async 1 -i default -c:v libx264 -preset ultrafast -vsync 1 ~/Videos/$filename.mp4
