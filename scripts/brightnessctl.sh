4060668108d1d520a0999262636c8a6986ae1c31
s0la <sola931@gmail.com>
2020-04-06 22:37:18 +0100

    Add flags for getting and setting brightness level

--- scripts/brightnessctl.sh
+++ scripts/brightnessctl.sh
@@ -4,7 +4,12 @@ path=/sys/class/backlight/*/
 read brightness_step <<< $(awk '{ print int($1 / 100) }' $path/max_brightness)
 read brightness_level <<< $(awk '{ print int($1 / '$brightness_step') }' $path/brightness)
 
-[[ $1 =~ [+-] ]] && new_level=$((brightness_level $1)) || new_level=$1
-((new_level *= brightness_step))
+case ${1#-} in
+	g) echo $brightness_level;;
+	s)
+		[[ $2 =~ [+-] ]] && new_level=$((brightness_level $2)) || new_level=$2
+		((new_level *= brightness_step))
 
-sudo echo "$new_level" > $path/brightness
+		sudo echo "$new_level" > $path/brightness
+esac
+
