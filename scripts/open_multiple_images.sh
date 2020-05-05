9ae796c9e48ad5142a83e54b47ecf9b6d8ba18f6
s0la <sola931@gmail.com>
2020-05-05 18:28:24 +0100

    Hide info bar

--- scripts/open_multiple_images.sh
+++ scripts/open_multiple_images.sh
@@ -8,4 +8,4 @@ while read -r wallpaper; do
 	[[ "$wallpaper" == "$start_wall" ]] && start_wall_index=$index
 done <<< $(find "${start_wall%/*}" -type f -iregex ".*\(jpe?g\|png\)" | sort)
 
-sxiv -n $start_wall_index ${walls[*]}
+sxiv -bn $start_wall_index ${walls[*]}
