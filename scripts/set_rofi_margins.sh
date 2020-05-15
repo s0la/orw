1202fecae044bc8538df89cb9f3307fba3af6461
s0la <sola931@gmail.com>
2020-05-15 06:56:52 +0100

    Fix config path

--- scripts/set_rofi_margins.sh
+++ scripts/set_rofi_margins.sh
@@ -2,10 +2,10 @@
 
 set_margins() {
 	[[ $@ ]] && ((index++))
-	sed -i "/#window/,/^$/ {/margin/ s/[0-9]\+/${1:-0}/${index-g}}" .config/rofi/dmenu.rasi
+	sed -i "/#window/,/^$/ {/margin/ s/[0-9]\+/${1:-0}/${index-g}}" ~/.config/rofi/dmenu.rasi
 }
 
-if [[ $(sed -n '$s/.*"\(.*\)"/\1/p' .config/rofi/main.rasi) == dmenu ]]; then
+if [[ $(sed -n '$s/.*"\(.*\)"/\1/p' ~/.config/rofi/main.rasi) == dmenu ]]; then
 	read x_offset y_offset <<< $(awk '/offset/ { print $NF }' ~/.config/orw/config | xargs)
 
 	while read -r bar_name position bar_x bar_y bar_width bar_height adjustable_width frame; do
