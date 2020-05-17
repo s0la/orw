74106740307489481183e7f28110a149963d081f
s0la <sola931@gmail.com>
2020-05-17 16:14:16 +0100

    Fix awk formatting

--- scripts/execute_on_terminal_startup.sh
+++ scripts/execute_on_terminal_startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-until [[ $(wmctrl -l | awk '$NF == "'$1'" {print "running"}') ]]; do continue; done
+until [[ $(wmctrl -l | awk '$NF == "'$1'" { print "running" }') ]]; do continue; done
 shift
 eval "$@"
