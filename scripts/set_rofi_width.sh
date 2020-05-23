2d457ff365ed3d454b48e9715bdd82c45b7e64d4
s0la <sola931@gmail.com>
2020-05-23 23:57:16 +0100

    Add new approach for counting items

--- scripts/set_rofi_width.sh
+++ scripts/set_rofi_width.sh
@@ -4,17 +4,21 @@ script=~/.orw/scripts/rofi_scripts/$1.sh
 theme=$(awk -F '"' 'END { print $(NF - 1) }' ~/.config/rofi/main.rasi)
 
 count_items() {
-	awk '/^\s*'"$1"'/ { wc = gensub(/[\0-\177]/, "", "g"); print length(wc) }' $script
+	awk '/s*'"$1"'/ { wc = gensub(/[\0-\177]/, "", "g"); print length(wc) }' $script
 }
 
 if [[ $theme == icons ]]; then
 	#item_count=$($script | wc -l)
 
-	case $1 in
-		wallpapers) item_count=$(count_items icons);;
-		workspaces) item_count=$(count_items 'workspaces=\(.*\)');;
-		*) item_count=$(awk '/<<-/ { start = 1; nr = NR + 1 } /^\s*EOF/ && start { print NR - nr; exit }' $script)
-	esac
+	if [[ $2 ]]; then
+		item_count=$2
+	else
+		case $1 in
+			wallpapers) item_count=$(count_items icons);;
+			workspaces) item_count=$(count_items 'workspaces=\(.*\)');;
+			*) item_count=$(awk '/<<-/ { start = 1; nr = NR + 1 } /^\s*EOF/ && start { print NR - nr; exit }' $script)
+		esac
+	fi
 
 	width=$(awk '
 		BEGIN { ic = '$item_count' }
@@ -26,7 +30,7 @@ if [[ $theme == icons ]]; then
 		/font/ { fs = get_value() }
 		/spacing/ { s = get_value() }
 		/padding/ { if(wp) ep = get_value(); else wp = get_value() }
-		END { print 2 * wp + int(ic * (2 * ep + s + fs * 1.37)) - s }' ~/.config/rofi/icons.rasi)
+		END { print 2 * wp + int(ic * (2 * ep + s + fs * 1.4)) - s }' ~/.config/rofi/icons.rasi)
 
 	#~/.orw/scripts/notify.sh "ic: $item_count   w: $width    $script"
 	sed -i "/width/ s/[0-9]\+/$((width + 0))/" ~/.config/rofi/icons.rasi
