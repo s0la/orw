dc1d542d89201d12a8bb0a88662eafd4c29880ec
s0la <sola931@gmail.com>
2020-05-24 00:01:27 +0100

    Tile remaining windows after closing

--- scripts/execute_on_terminal_startup.sh
+++ scripts/execute_on_terminal_startup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-until [[ $(wmctrl -l | awk '$NF == "'$1'" { print "running" }') ]]; do continue; done
+(($(sed -n '/class.*\*/=' ~/.config/openbox/rc.xml))) &&
+	close='&& ~/.orw/scripts/get_window_neighbours.sh'
+
+#until [[ $(wmctrl -l | awk '$NF == "'$1'" { print "running" }') ]]; do continue; done
+until [[ $(wmctrl -l | awk '$NF == "'$1'"') ]]; do continue; done
 shift
-eval "$@"
+eval "$@" $close
