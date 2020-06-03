1844edcfe3dc66e4c503c505d4333f4ed53f2c3d
s0la <sola931@gmail.com>
2020-06-03 02:13:09 +0100

    Separate logic for handeling different tiling modes

--- scripts/close_window.sh
+++ scripts/close_window.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-id=$1
-ids_to_tile="$2"
+id=$2
+ids="$3"
+mode=$1
 
 while [[ $(wmctrl -l | awk '$1 == "'$id'"') ]]; do continue; done
 #while
@@ -10,6 +11,12 @@ while [[ $(wmctrl -l | awk '$1 == "'$id'"') ]]; do continue; done
 #	sleep 0.1
 #done
 
-for id in $ids_to_tile; do
-	~/.orw/scripts/windowctl.sh -i $id tile $orientation
-done
+if [[ $mode == auto ]]; then
+	for id in $ids; do
+		~/.orw/scripts/windowctl.sh -i $id tile $orientation &> /dev/null
+	done
+else
+	[[ $4 == h ]] && direction=L || direction=T
+	#~/.orw/scripts/notify.sh "${ids%% *} move -$direction -A ${4}c"
+	~/.orw/scripts/windowctl.sh -i ${ids%% *} move -$direction -A ${4}c &> /dev/null
+fi
--- scripts/get_window_neighbours.sh
+++ scripts/get_window_neighbours.sh
@@ -3,20 +3,29 @@
 id=$(printf "0x%.8x" $(xdotool getactivewindow))
 properties=( $(wmctrl -lG | awk '$1 == "'$id'" { print $NF, $3, $4, $5, $6 }') )
 
-mode=$(awk '/class.*\*/ { print "tiling" }' ~/.config/openbox/rc.xml)
+#mode=$(awk '/class.*\*/ { print "tiling" }' ~/.config/openbox/rc.xml)
+mode=$(awk '/^mode/ { print $NF }' ~/.config/orw/config)
 
-if [[ $mode == tiling ]]; then
+if [[ $mode != floating ]]; then
 	get_ids() {
 		[[ $1 == h ]] && index=3 start_index=2 || index=4 start_index=1
 		desktop=$(xdotool get_desktop)
 		start=${properties[start_index]}
 		end=$((start + ${properties[start_index + 2]}))
 
+		#wmctrl -lG | sort -nk $index,$index | awk '\
 		read $1_size $1_ids <<< $(wmctrl -lG | sort -nk $index,$index | awk '\
 				function set_current_window() {
 					cp = p
 					cd = d
 					cid = $1
+					dis = (p < ws) ? ws - (p + $('$index' + 2)) : p - we
+					#dis = ($'$index' < ws) ? ws - ($'$index' + $('$index' + 2)) : $'$index' - we
+				}
+
+				BEGIN {
+					ws = '${properties[index - 2]}'
+					we = ws + '${properties[index]}'
 				}
 
 				$2 == '$desktop' && $1 != "'$id'" {
@@ -33,9 +42,12 @@ if [[ $mode == tiling ]]; then
 								cid = cid " " $1
 							} else {
 								#system("~/.orw/scripts/notify.sh \"" max "\"")
-								if(cd >= max) {
+								#if(cd >= max && dis <= md) {
+								if(cd >= max && (!md || dis < md)) {
 									max = cd
+									md = dis
 									id = cid
+									mp = p
 									set_current_window()
 								}
 							}
@@ -43,7 +55,7 @@ if [[ $mode == tiling ]]; then
 							set_current_window()
 						}
 					}
-				} END { print (cd >= max) ? cd " " cid : max " " id }')
+				} END { print (cd >= max && (!md || dis < md)) ? cd " " cid : max " " id }')
 	}
 
 	get_ids h
@@ -62,7 +74,7 @@ fi
 #  done
 #} &
 
-[[ $ids ]] && nohup ~/.orw/scripts/close_window.sh $id "${!ids}" &
+[[ $ids ]] && nohup ~/.orw/scripts/close_window.sh $mode $id "${!ids}" ${ids:0:1} &
 wmctrl -ic $id
 
 name=${properties[0]}
