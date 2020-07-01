a9017d3a27779efe2e756c01db6e46af78034bf4
s0la <sola931@gmail.com>
2020-07-01 22:43:21 +0000

    Move tiling toggle to tiling group

--- scripts/rofi_scripts/tiling_group.sh
+++ scripts/rofi_scripts/tiling_group.sh
@@ -4,6 +4,7 @@ path=~/.orw/scripts/rofi_scripts
 ${path%/*}/set_rofi_width.sh tiling_$1
 ${path%/*}/set_rofi_margins.sh
 
-modis+="ratio:$path/tiling_ratio.sh,"
-modis+="offset:$path/tiling_offset.sh"
+modis+="toggle:$path/tiling_toggle.sh,"
+modis+="offset:$path/tiling_offset.sh,"
+modis+="ratio:$path/tiling_ratio.sh"
 rofi -modi "$modis" -show $1 -theme main
