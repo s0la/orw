157171ebe8b429096ff92c4ba64e31b49640d4e5
s0la <sola931@gmail.com>
2020-07-01 22:25:03 +0000

    Add block icon representation

--- scripts/bar/workspaces.sh
+++ scripts/bar/workspaces.sh
@@ -32,73 +32,50 @@ for workspace_index in $(seq $workspace_count); do
 					[[ $value == p ]] && offset=$padding || offset='$inner'
 				fi;;
 			[cr]) [[ ! $flags =~ $arg ]] && flags+=$arg;;
-			s*) workspace_separator="\$bsbg%{O${arg:1}}";;
+			s*) workspace_separator="\$jbg%{O${arg:1}}";;
 			l) label="${padding}$(wmctrl -d | awk '$1 == '$((workspace_index - 1))' \
 				{ wn = $NF; if(wn ~ /^[0-9]+$/) { if(wn > 1) tc = wn - 1; wn = "tmp" tc }; print wn }')${padding}";;
 			n) label="$offset$workspace_index$offset";;
 			b*) ((${#arg} > 1)) && label=%{O${arg:1}} || label=$offset;;
 			*)
-				case ${arg: -1} in
+				case ${arg:1} in
 					d) icon_type=dot;;
 					h) icon_type=half;;
 					e) icon_type=empty;;
+					b) icon_type=block;;
+					be) icon_type=block_empty;;
 					*) icon_type=default;;
 				esac
 
 				icon="$(sed -n "s/Workspace_${icon_type}_${current}_icon=//p" ${0%/*}/icons)"
-				#~/.orw/scripts/notify.sh "Workspace_${icon_type}_${current}_icon"
-				#:icon="${current}_icon"
-				#~/.orw/scripts/notify.sh "$workspace_index: $offset"
-				#~/.orw/scripts/notify.sh "$workspace_index: $offset"
 				label="$offset$icon$offset";;
 		esac
 	done
 
 	bg="\${W${current}bg:-\${Wsbg:-\$${current}bg}}"
 	fg="\${W${current}fg:-\${Wsfg:-\$${current}fg}}"
-
 	[[ $fbg ]] || fbg=$bg
 
 	command="wmctrl -s $((workspace_index - 1)) \&\& ~/.orw/scripts/barctl.sh -b wss -k \&"
 	[[ $flags ]] && command+=" ~/.orw/scripts/wallctl.sh -$flags \&"
-	#~/.orw/scripts/notify.sh "$command"
 	workspace="%{A:$command:}$bg$fg$label%{A}"
-
-	#if [[ $single_line == true && $current == p ]]; then
-	#	set_line
-	#	workspace="%{U$fc}\${start_line:-$left_frame}$workspace\${end_line:-$right_frame}"
-	#fi
-
 
 	if [[ $single_line == true && $separator =~ ^% && $current == p ]]; then
 		set_line
 		workspace="%{U$fc}\${start_line:-$left_frame}$workspace\${end_line:-$right_frame}"
 	fi
 
-
-	#if [[ $single_line == true ]]; then
-	#	if [[ $current == p ]]; then
-	#		set_line
-
-	#		[[ ! $separator =~ ^[s%] ]] && workspace="\$start_line$workspace\$end_line" ||
-	#			workspace="%{U$fc}\${start_line:-$left_frame}$workspace\${end_line:-$right_frame}"
-	#	else
-	#		workspace="%{-o}%{-u}$workspace"
-	#	fi
-	#fi
-
 	((workspace_index < workspace_count)) && workspace+="$workspace_separator"
-
 	workspaces+="$workspace"
 done
 
 [[ $4 =~ i ]] && workspaces="$fbg${padding}$workspaces${padding}"
+#[[ $4 =~ i[^b] ]] && workspaces="$fbg${padding}$workspaces${padding}"
 #[[ $separator =~ ^% ]] && workspaces="$remove_frame$workspaces"
 
 workspaces="%{A2:~/.orw/scripts/barctl.sh -b wss:}\
 %{A4:wmctrl -s $((((current_workspace + workspace_count - 2) % workspace_count))):}\
-%{A5:wmctrl -s $((current_workspace % workspace_count)):}\
-$workspaces%{A}%{A}%{A}"
+%{A5:wmctrl -s $((current_workspace % workspace_count)):}$workspaces%{A}%{A}%{A}"
 
 if [[ $single_line == true ]]; then
 	#~/.orw/scripts/notify.sh "s: $separator"
