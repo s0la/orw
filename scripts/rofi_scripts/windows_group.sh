38add644fd484461f2c87801bbf70431d89eb100
s0la <sola931@gmail.com>
2020-07-31 19:42:34 +0000

    Remove tiling toggle

--- scripts/rofi_scripts/windows_group.sh
+++ scripts/rofi_scripts/windows_group.sh
@@ -5,7 +5,6 @@ ${path%/*}/set_rofi_width.sh window_$1
 ${path%/*}/set_rofi_margins.sh
 
 modis+="actions:$path/window_actions.sh,"
-modis+="toggle:$path/window_toggle.sh,"
 #modis+="ratio:$path/window_ratio.sh,"
 #modis+="offset:$path/window_offset.sh,"
 modis+="managment:$path/window_managment.sh"
