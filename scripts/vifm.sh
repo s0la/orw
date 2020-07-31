1d426599f51422d84646e087124d5e732fd834fa
s0la <sola931@gmail.com>
2020-07-31 19:43:56 +0000

    Calculate title if not specified

--- scripts/vifm.sh
+++ scripts/vifm.sh
@@ -19,6 +19,8 @@ run_command=~/.config/vifm/scripts/run_with_image_preview
 if [[ $instance ]]; then
 	~/.orw/scripts/set_geometry.sh -c size -w ${width:-400} -h ${height:-500}
 
+	[[ ! $title ]] && title=$(wmctrl -l | awk '$NF ~ "^vifm[0-9]+?" { c++ } END { print "vifm" c }')
+
 	termite -t ${title-vifm} --class=${class-custom_size} -e \
 		"bash -c '$run_command $args'" &> /dev/null &
 else
