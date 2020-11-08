454d30113c16ea962b49bfa7e19a785496b80260
s0la <sola931@gmail.com>
2020-11-08 16:19:08 +0000

    Make close button same as other buttons

--- scripts/bar/power.sh
+++ scripts/bar/power.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-colorscheme=$4
+colorscheme=~/.config/orw/colorschemes/$4.ocs
 close_offset=20
 close_font="icomoon_material_tile:size=10"
 close_font="remix:size=15"
@@ -17,18 +17,19 @@ for arg in ${3//,/ }; do
 			eval $(sed -n 's/power_\(.*=\)[^}]*.\(.\).*/\1\2/p' ~/.orw/scripts/bar/icons);;
 		#[0-9]*) [[ $arg =~ x ]] && width_ratio=${arg%x*} height_ratio=${arg#*x} || equal_ratio=$arg;;
 		s[0-9]*) main_font_size=${arg:1};;
+		o[0-9]*) offset="%{${arg^}}";;
 		*)
 			action_count=${#arg}
 
 			for action_index in $(seq ${#arg}); do
-				((action_index - 1)) && actions+='%{O$separator}'
+				[[ ! $offset ]] && ((action_index % ${#arg})) && actions+='%{O$separator}'
 
 				case ${arg:action_index - 1:1} in
-					l) actions+="%{A:kill "$pid" && openbox --exit:}${logout_icon:-logout}%{A}";;
-					r) actions+="%{A:kill "$pid" && systemctl reboot:}${reboot_icon:-reboot}%{A}";;
-					s) actions+="%{A:kill "$pid" && systemctl suspend:}${suspend_icon:-suspend}%{A}";;
-					o) actions+="%{A:kill "$pid" && systemctl poweroff:}${power_off_icon:-power off}%{A}";;
-					L) actions+="%{A:kill "$pid" && ~/.orw/scripts/lock_screen.sh:}${lock_icon:-lock}%{A}";;
+					l) actions+="$offset%{A:kill "$pid" && openbox --exit:}${logout_icon:-logout}%{A}$offset";;
+					r) actions+="$offset%{A:kill "$pid" && systemctl reboot:}${reboot_icon:-reboot}%{A}$offset";;
+					s) actions+="$offset%{A:kill "$pid" && systemctl suspend:}${suspend_icon:-suspend}%{A}$offset";;
+					o) actions+="$offset%{A:kill "$pid" && systemctl poweroff:}${power_off_icon:-power off}%{A}$offset";;
+					L) actions+="$offset%{A:kill "$pid" && ~/.orw/scripts/lock_screen.sh:}${lock_icon:-lock}%{A}$offset";;
 				esac
 			done
 	esac
@@ -37,7 +38,8 @@ done
 main_font="${main_font_type:-Iosevka Orw}:size=${main_font_size:-9}"
 #~/.orw/scripts/notify.sh "mf: $main_font"
 
-read geometry offset separator <<< $(awk -F '[_ ]' '{
+#read geometry offset separator <<< $(awk -F '[_ ]' '{
+read geometry separator <<< $(awk -F '[_ ]' '{
 			if(/^orientation/ && $NF ~ /^v/) v = 1
 
 			if($1 == "primary") {
@@ -61,27 +63,44 @@ read geometry offset separator <<< $(awk -F '[_ ]' '{
 					o = int(h / 2 - '$close_offset')
 					x += int(($3 - w) / 2)
 					y = int(($4 - h) / 2)
-					print w "x" h "+" x "+" y, o, s
+					#print w "x" h "+" x "+" y, o, s
+					print w "x" h "+" x "+" y, s
 					exit
 				}
 			}
 		}' ~/.config/orw/config)
 
-eval $(awk '\
-	/#bar/ { 
-		nr = NR
-		b = '${base:-0}'
-	} nr && NR > nr {
-		if($1 ~ "^(b?bg|.*c)$") c = $2
-		else {
-			l = length($1)
-			p = substr($1, l - 1, 1)
-			c = "%{" toupper(p) $2 "}"
-		}
+#eval $(awk '\
+#	/#bar/ { 
+#		nr = NR
+#		b = '${base:-0}'
+#	} nr && NR > nr {
+#		if($1 ~ "^(b?bg|.*c)$") c = $2
+#		else {
+#			l = length($1)
+#			p = substr($1, l - 1, 1)
+#			c = "%{" toupper(p) $2 "}"
+#		}
+#
+#		if($1) print $1 "=\"" c "\""
+#	} nr && (/^$/ || (b && NR > nr + b)) { exit }' ~/.config/orw/colorschemes/$colorscheme.ocs)
 
-		if($1) print $1 "=\"" c "\""
-	} nr && (/^$/ || (b && NR > nr + b)) { exit }' ~/.config/orw/colorschemes/$colorscheme.ocs)
+eval $(awk '$1 ~ "^(P?s[bf]g|.*c)" { print gensub(" ", "=", 1) }' $colorscheme)
 
 close="%{A:kill "$pid":} %{A}"
-eval "all=\"$pfg%{c}$actions%{r}%{T2}$sfg$close%{O$close_offset}\""
-echo -e "$all" | lemonbar -B $bg -p -g $geometry -f "$main_font" -o 0 -f "$close_font" -o -$offset -n power_bar | bash
+actions+="$offset%{A:kill "$pid":}${close_icon:-close}%{A}$offset"
+
+#~/.orw/scripts/notify.sh "bg: $bg"
+bg=${Psbg:-$sbg}
+fg=${Psfg:-$sfg}
+#~/.orw/scripts/notify.sh "bg: $bg"
+#echo -e "$actions" > ~/Desktop/pow_bar
+
+eval "content=\"%{c}%{B$bg}%{F$fg}$actions\""
+#eval "all=\"$pfg%{c}$actions%{r}%{T2}$sfg$close%{O$close_offset}\""
+#echo -e "$all" | lemonbar -B $bg -p -g $geometry -f "$main_font" -o 0 -f "$close_font" -o -$offset -n power_bar | bash
+
+#echo -e "%{c}$actions" | \
+echo -e "$content" | \
+	lemonbar -d -p -B $bg -F $fg -R ${Pfc:-$fc} -r 3\
+	-f "$main_font" -o 0 -g $geometry -n power_bar | bash
