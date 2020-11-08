aef8fbd3e78c1f51b6a6cd85f3cf43d92ddd7d76
s0la <sola931@gmail.com>
2020-11-08 18:22:19 +0000

    Autostart tiling script if wm is in tiling mode

--- dotfiles/.config/openbox/autostart.sh
+++ dotfiles/.config/openbox/autostart.sh
@@ -5,12 +5,16 @@ xset s blank
 xset s 1000
 
 #compton --config /dev/null &
-picom --experimental-backends -b &> /dev/null &
+picom -b
+#picom --experimental-backends -b &> /dev/null
 mpd &
 
 path=~/.orw/scripts
 $path/wallctl.sh -r &
 $path/mpd_notifier.sh &
 $path/barctl.sh
+
+mode=$(awk '/^mode/ { print $NF }' ~/.config/orw/config)
+[[ $mode == floating ]] || $path/tile_windows.sh &
 
 xrdb -load ~/.config/X11/xresources
