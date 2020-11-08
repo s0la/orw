48980dec84d46e4e29d29cc6e2d56f407b9e7db9
s0la <sola931@gmail.com>
2020-11-08 17:34:28 +0000

    Use terminal colors

--- scripts/screenkey.sh
+++ scripts/screenkey.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+read fg bg <<< $(awk '\
+	$1 == "background" {
+		argb = gensub(".*\\(([0-9,]*),(.*[0-9]).*", "\\2,\\1", 1)
+		split(argb, argba, ",")
+		printf "#%.2x%.2x%.2x\n", argba[2], argba[3], argba[4]
+	}
+	$1 == "foreground" { print $NF }' ~/.config/termite/config | xargs)
+
 [[ $1 == -k ]] && killall screenkey ||
-	screenkey -g 500x400+710+650 -t 1.2 -s medium --bg-color \#303030 --font-color \#a0a0a0
+	screenkey -g 500x400+710+650 -t 1.2 -s medium --bg-color $bg --font-color $fg
