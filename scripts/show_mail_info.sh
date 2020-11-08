12d375e5db7872eaa172618b203b9e34587fa8b1
s0la <sola931@gmail.com>
2020-11-08 17:36:08 +0000

    Add optional argument for title length

--- scripts/show_mail_info.sh
+++ scripts/show_mail_info.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 count=$3
+length=${4:-55}
 
 while read -r mail_info; do
 	all_mail_info+="$mail_info\n\n"
 done <<< $(curl -u "$1":"$2" --silent "https://mail.google.com/mail/feed/atom" |
 	xmllint --format - 2> /dev/null | awk -F '[<>]' '
-		/name|title/ && ! /Inbox/ {
+		/^ *<(name|title)/ && ! /Inbox/ {
 			if('$count' && c && c > '$count') exit
 			else if($2 == "title" && $3) {
 				c++
-				e = (length($3) > 80) ? ".." : ""
-				t = sprintf("\\n%.80s%s", $3, e)
+				l = '$length'
+				e = (length($3) > l) ? ".." : ""
+				t = sprintf("\\n%.*s%s", l, $3, e)
 			} else print "<b>" $3 "</b>" t }' )
 
-~/.orw/scripts/notify.sh -f 8 -t 10 -p "$all_mail_info"
+~/.orw/scripts/notify.sh -f 8 -t 10 -p "\n$all_mail_info"
