99d4ba508fb701ef8846302e3766c510d185b8ba
s0la <sola931@gmail.com>
2020-12-27 15:36:17 +0000

    Fix formating

--- scripts/show_top_usage.sh
+++ scripts/show_top_usage.sh
@@ -3,6 +3,7 @@
 while read -r propcess; do
 	top_processes+="$propcess\n"
 done <<< $(ps axch -o cmd,%$1 --sort=-%$1 | \
-	awk 'NR < 10 {cmds[$1] += $NF} END {for (cmd in cmds) printf("%-30s %5s\n", cmd, cmds[cmd])}' | sort -rnk 2)
+	awk 'NR < 10 { cmds[$1] += $NF } END { for (cmd in cmds) printf("%-30s %5s\n", cmd, cmds[cmd]) }' | \
+	sort -rnk 2)
 
 ~/.orw/scripts/notify.sh -p "<b>${1^} usage:</b>\n\n$top_processes"
