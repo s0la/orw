a84b1531de7c2177cf9c6863a8e63fb2a0cea0c3
s0la <sola931@gmail.com>
2020-12-27 16:52:29 +0000

    Remove comments

--- scripts/set_window_opacity.sh
+++ scripts/set_window_opacity.sh
@@ -3,13 +3,9 @@
 set_opacity() {
 	local opacity_hex=$(printf '0x%.2xffffff' $((0xff * $1 / 100)))
 	xprop -id ${id:-$2} -format _NET_WM_WINDOW_OPACITY 32c -set _NET_WM_WINDOW_OPACITY $opacity_hex
-	echo xprop -id ${id:-$2} -format _NET_WM_WINDOW_OPACITY 32c -set _NET_WM_WINDOW_OPACITY $opacity_hex >> ~/Desktop/opac
 }
 
 if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
-	#[[ $1 =~ ^0x ]] &&
-	#	id=$1 || id=$(printf '0x%.8x' $(xdotool getactivewindow))
-
 	[[ $2 ]] || id=$(printf '0x%.8x' $(xdotool getactivewindow))
 	set_opacity ${2:-$1} $1
 fi
