1944acd00d191f3b6a4baf198a9292715c466c8e
s0la <sola931@gmail.com>
2020-12-27 15:01:18 +0000

    Refactor geometry calculation and add mode flag

--- scripts/set_rofi_geometry.sh
+++ scripts/set_rofi_geometry.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+getopts :m: flag
+[[ $flag ]] && mode=$OPTARG && shift 2
+
 id=$(xdotool getactivewindow 2> /dev/null)
-theme=$(awk -F '"' 'END { print $(NF - 1) }' ~/.config/rofi/main.rasi)
+[[ $mode ]] || mode=$(awk -F '"' 'END { print $(NF - 1) }' ~/.config/rofi/main.rasi)
 
 if [[ $id ]]; then
 	read x y <<< $(wmctrl -lG | awk '$1 == sprintf("0x%.8x", "'$id'") { print $3, $4 }')
@@ -10,78 +13,83 @@ else
 	read width height <<< $(awk '/^primary/ { p = $NF } p && $1 == p { print $2, $3 }' ~/.config/orw/config)
 fi
 
-if [[ $theme == icons ]]; then
+if [[ $mode == icons ]]; then
 	script=~/.orw/scripts/rofi_scripts/dmenu/$1.sh
 
 	count_items() {
 		awk '/s*'"$1"'/ { wc = gensub(/[\0-\177]/, "", "g"); print length(wc) }' $script
 	}
 
-	if [[ $2 ]]; then
-		item_count=$2
-	else
-		case $1 in
-			#wallpapers) item_count=6;;
-			workspaces) item_count=$(wmctrl -d | awk 'END { print NR + 1 }');;
-			*) item_count=$(awk '/<<-/ { start = 1; nr = NR + 1 } /^\s*EOF/ && start { print NR - nr; exit }' $script)
-		esac
-	fi
+	[[ $2 ]] &&
+		item_count=$2 ||
+		item_count=$(awk '\
+			/<<-/ { start = 1; nr = NR + 1 }
+			/^\s*EOF/ && start { print NR - nr; exit }' $script)
+
+	#if [[ $2 ]]; then
+	#	item_count=$2
+	#else
+	#	case $1 in
+	#		#wallpapers) item_count=6;;
+	#		workspaces) item_count=$(wmctrl -d | awk 'END { print NR + 2 }');;
+	#		*) item_count=$(awk '/<<-/ { start = 1; nr = NR + 1 } /^\s*EOF/ && start { print NR - nr; exit }' $script)
+	#	esac
+	#fi
 
 	#awk -i inplace -F '[ %;]' '{
-	awk -i inplace -F '[ ;]' '{
-		#w = '$width'
-		#h = '$height'
-		ic = '$item_count'
+	awk -i inplace -F '[ ;]' '\
+		BEGIN { ic = '$item_count' }
 
-		if(/font/) fs = gensub(/.* ([0-9.]+).*/, "\\1", 1)
+		{
+			if(/font/) fs = gensub(/.* ([0-9.]+).*/, "\\1", 1)
 
-		if(/window-orientation:/) o = $(NF - 1)
+			if(/window-orientation:/) o = $(NF - 1)
 
-		if(/(window|element)-padding:/) {
-			hp[++vi] = gensub(/[^0-9]*( ([0-9]+)px){2}.*/, "\\2", 1) * 2
-			vp[++hi] = gensub(/[^0-9]*( ([0-9]+)px){1}.*/, "\\2", 1) * 2
-		}
-
-		if(/element-border:/) eb = gensub(/.* ([0-9]+)px.*/, "\\1", 1)
-
-		if(/list-spacing:/) ls = gensub(".* ([0-9.]+)px.*", "\\1", 1)
-
-		if(/window-orientation:/) o = $(NF - 1)
-
-		if(/window-margin:/) wm = gensub(/.* ([0-9]+)px.*/, "\\1", 1)
-
-		if(/window-width:/) {
-			if(o == "vertical") {
-				fw = fs *1.37
-				tw = hp[2] + hp[1] + fw + wm
-			} else {
-				#for three consecutive different font sizes
-				fm = fs % 3
-				fw = (fm == 2) ? 1.291 + (1 / fs) : (fm == 1) ? 1.31 : 1.33
-				#for reducing font width as count grows
-				fw *= (fs + (fs / ic) / 10)
-				tw = hp[2] + (hp[1] + fw) * ic + ls * (ic - 1)
+			if(/(window|element)-padding:/) {
+				hp[++vi] = gensub(/[^0-9]*( ([0-9]+)px){2}.*/, "\\2", 1) * 2
+				vp[++hi] = gensub(/[^0-9]*( ([0-9]+)px){1}.*/, "\\2", 1) * 2
 			}
 
-			sub(/[0-9.]+px/, tw "px")
-		}
+			if(/element-border:/) eb = gensub(/.* ([0-9]+)px.*/, "\\1", 1)
 
-		if(/window-height:/) {
-			if(o == "vertical") {
-				#fh = (fs % 2 == 0) ? fs * 1.51 : fs * 1.48
-				fh = (fs % 2 == 0) ? fs * 1.55 : fs * 1.55
-				th = vp[2] + (vp[1] + fh) * ic + ls * (ic - 1) + eb
-			} else {
-				#fh = fs * 2.37
-				#fh = fs * (3 - (0.11 - (fs / 1000)) * int((fs + 1) / 2))
-				#th = vp[2] + vp[1] + fh + eb
-				fh = fs * 1.5
-				th = vp[2] + vp[1] + fh + eb + wm
+			if(/list-spacing:/) ls = gensub(".* ([0-9.]+)px.*", "\\1", 1)
+
+			if(/window-orientation:/) o = $(NF - 1)
+
+			if(/window-margin:/) wm = gensub(/.* ([0-9]+)px.*/, "\\1", 1)
+
+			if(/window-width:/) {
+				if(o == "vertical") {
+					fw = fs *1.37
+					tw = hp[2] + hp[1] + fw + wm
+				} else {
+					#for three consecutive different font sizes
+					fm = fs % 3
+					fw = (fm == 2) ? 1.291 + (1 / fs) : (fm == 1) ? 1.31 : 1.33
+					#for reducing font width as count grows
+					fw *= (fs + (fs / ic) / 10)
+					tw = hp[2] + (hp[1] + fw) * ic + ls * (ic - 1)
+				}
+
+				sub(/[0-9.]+px/, tw "px")
 			}
 
-			sub(/[0-9.]+px/, th "px")
-		}
-	} { print }' ~/.config/rofi/$theme.rasi
+			if(/window-height:/) {
+				if(o == "vertical") {
+					#fh = (fs % 2 == 0) ? fs * 1.51 : fs * 1.48
+					fh = (fs % 2 == 0) ? fs * 1.55 : fs * 1.55
+					th = vp[2] + (vp[1] + fh) * ic + ls * (ic - 1) + eb
+				} else {
+					#fh = fs * 2.37
+					#fh = fs * (3 - (0.11 - (fs / 1000)) * int((fs + 1) / 2))
+					#th = vp[2] + vp[1] + fh + eb
+					fh = fs * 1.5
+					th = vp[2] + vp[1] + fh + eb + wm
+				}
+
+				sub(/[0-9.]+px/, th "px")
+			}
+		} { print }' ~/.config/rofi/$mode.rasi
 else
 	#read x_offset y_offset <<< $(awk '/^[xy]_offset/ { print $NF }' ~/.config/orw/config | xargs)
 
@@ -92,11 +100,12 @@ else
 			fi
 
 			bar_end=$((bar_x + bar_width + frame))
-			current_bar_height=$((bar_y + bar_height + frame + 2))
+			#current_bar_height=$((bar_y + bar_height + frame + 2))
+			bar_y_end=$((bar_y + bar_height + frame))
 
 			((x_offset && bar_x > x_offset)) || x_offset=$bar_x
 			((bar_end > right_x_offset)) && right_x_offset=$bar_end
-			((current_bar_height > y_offset)) && y_offset=$current_bar_height
+			((bar_y_end > bar_max_y_end)) && bar_max_y_end=$bar_y_end y_offset=$bar_y
 			#(((x_offset && x_offset > bar_x) || !x_offset)) && x_offset=$bar_x
 
 			#if ((x_offset)); then
@@ -117,14 +126,17 @@ else
 		/window-width:/ {
 			#rw = ('$((right_x_offset - x_offset))') / ('$width' / 100)
 			rw = '$((right_x_offset - x_offset))'
-			sub(/[0-9]+/, rw)
+			sub(/[0-9]+[^;]*/, rw "px")
 		}
 
 		/window-margin:/ {
 			#wm = '$y_offset' / ('$height' / 100)
 			#sub(/[0-9.]+%/, wm "%")
-			sub(/[0-9]+px/, '$y_offset' "px")
+			by = '${bar_y:-0}'
+			bye = '${bar_max_y_end:-0}'
+			y = (by) ? bye + by : bye
+			sub(/[0-9]+px/, y "px")
 		}
 
-		{ print }' ~/.config/rofi/$theme.rasi
+		{ print }' ~/.config/rofi/$mode.rasi
 fi
