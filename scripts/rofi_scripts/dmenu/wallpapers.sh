5cb38de831d522c80f4e898853c086eafed8f46d
s0la <sola931@gmail.com>
2021-01-16 22:22:24 +0000

    Add wallpaper category selector and adjust icons

--- scripts/rofi_scripts/dmenu/wallpapers.sh
+++ scripts/rofi_scripts/dmenu/wallpapers.sh
@@ -13,7 +13,7 @@ get_directory() {
 if [[ $theme == icons ]]; then
 	#auto=$(systemctl --user status change_wallpaper.timer | awk '/Active/ { print ($2 == "active") ? "" : "" }')
 	#auto=$(systemctl --user status change_wallpaper.timer | awk '/Active/ { if($2 == "active") print "-a 5" }')
-	active=$(systemctl --user is-active change_wallpaper.timer | awk '{ if(/^active/) print "-a 6" }')
+	active=$(systemctl --user is-active change_wallpaper.timer | awk '{ if(/^active/) print "-a 4" }')
 	prev= next= rand= restore= view= 
 	prev= next= rand= restore= view= select= auto=
 	prev= next= rand= restore= view= select= auto=
@@ -21,8 +21,18 @@ if [[ $theme == icons ]]; then
 	prev= next= rand= restore= view= select= auto=
 	prev= next= rand= restore= view= select= auto=
 	prev= next= rand= restore= view= select= auto=
+	prev= next= rand= restore= view= select= auto=
+	prev= next= rand= restore= select= categories= view= auto=
+	prev= next= rand= restore= select= categories= view= auto=
+	prev= next= rand= restore= select= categories= view= auto=
+	prev= next= rand= restore= select= categories= view= auto=
+	prev= next= rand= restore= select= categories= view= auto=
+	prev= next= rand= restore= select= categories= view= auto=
+	prev= next= rand= restore= select= categories= view= auto=
+	prev= next= rand= restore= select= categories= view= auto=
+	prev= next= rand= restore= select= categories= view= auto=
 else
-	next=next prev=prev rand=rand restore=restore view=view_all select=select auto=autochange nl=\n
+	next=next prev=prev rand=rand restore=restore select=select categories=categories view=view auto=autochange nl=\n
 fi
 
 #action=$(echo -e "$prev\n$next\n$rand\n$view\n$index$nl$restore\n$interval$nl$auto" | rofi -dmenu $active -theme main)
@@ -42,10 +52,11 @@ list_actions() {
 		$prev
 		$rand
 		$next
-		$view
 		$restore
-		$select
 		$auto
+		$categories
+		$select
+		$view
 	EOF
 	)
 }
@@ -69,6 +80,9 @@ wallctl=~/.orw/scripts/wallctl.sh
 #fi
 
 list_actions
+
+#~/.orw/scripts/notify.sh "a: ^$action^ ^$categories^"
+#exit
 
 if [[ $action == $select ]]; then
 	indicator='●'
@@ -96,7 +110,9 @@ else
 				$restore*) $wallctl -r;;
 				$auto*) $wallctl -A;;
 				$view*) $wallctl -v;;
-				$interval*) $wallctl -I ${action#* };;
+				$categories*)
+					killall rofi
+					rofi -modi "categories:${0%/*}/wallpaper_category_selection.sh" -show categories -theme large_list;;
 				$index*) $wallctl -i ${action##* };;
 				*) $wallctl -o $action;;
 			esac
