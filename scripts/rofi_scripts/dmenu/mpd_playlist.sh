08d845125e065942859cdb1ce424825d0b449c17
s0la <sola931@gmail.com>
2021-05-16 17:15:50 +0200

    Add load/save/clear options to playlist

--- scripts/rofi_scripts/dmenu/mpd_playlist.sh
+++ scripts/rofi_scripts/dmenu/mpd_playlist.sh
@@ -7,11 +7,49 @@ song_index=$(mpc current -f %position%)
 current_song="$(mpc current -f "%artist% - %title%")"
 [[ $current_song ]] && empty='  '
 
-song=$(mpc playlist | awk '{
-	p = ($0 == "'"$current_song"'") ? "'$indicator' " : "'"$empty"'"
-	print p $0 }' | rofi -dmenu -i -selected-row $((song_index - 1)) -theme large_list)
+#song=$(mpc playlist | awk '{
+#	p = ($0 == "'"$current_song"'") ? "'$indicator' " : "'"$empty"'"
+#	print p $0 }' | rofi -dmenu -i -selected-row $((song_index - 1)) -theme large_list)
 
-[[ $song ]] && ~/.orw/scripts/play_song_from_playlist.sh "${song:${#empty}}"
+list_songs() {
+	mpc playlist | awk '
+		BEGIN { print "'"$empty"'options\n'"$empty"'━━━━━━━" }
+		{ p = ($0 == "'"$current_song"'") ? "'$indicator' " : "'"$empty"'"; print p $0 }'
+}
+
+get_item() {
+	item=$(list_songs | rofi -dmenu -i -selected-row $((song_index + 1)) -theme large_list)
+}
+
+#while true; do
+	get_item
+
+	if [[ ${item#$empty} == options ]]; then
+		option=$(echo -e 'back\nload\nsave\nclear\nrefresh' | rofi -dmenu -theme large_list)
+
+		case $option in
+			refresh) set_options;;
+			clear|save*)
+				mpc -q $option
+				action="${option%% *}ed"
+				[[ $option =~ ' ' ]] && playlist="${option#* }";;
+			load)
+				playlist=$(mpc lsplaylists | grep -v .*.m3u | rofi -dmenu -theme large_list)
+				action="loaded"
+				mpc load "$playlist" > /dev/null
+		esac
+
+		[[ $@ != load ]] && ~/.orw/scripts/notify.sh -p "<b>$playlist</b> playlist ${action//ee/e}."
+		get_item
+	fi
+
+	[[ $item ]] && ~/.orw/scripts/play_song_from_playlist.sh "${item:${#empty}}"
+	#~/.orw/scripts/play_song_from_playlist.sh "${item:${#empty}}"
+#done
+
+#[[ ${item#* } == refresh ]] && get_item
+#[[ $item ]] && ~/.orw/scripts/play_song_from_playlist.sh "${item:${#empty}}"
+#[[ $song ]] && ~/.orw/scripts/play_song_from_playlist.sh "${song:${#empty}}"
 
 #if [[ -z $@ ]]; then
 #	mpc playlist | awk '{
--- scripts/rofi_scripts/mpd_playlist.sh
+++ scripts/rofi_scripts/mpd_playlist.sh
@@ -1,16 +1,65 @@
 #!/bin/bash
 
-indicator=''
 indicator='●'
 
+song_index=$(mpc current -f %position%)
 current_song="$(mpc current -f "%artist% - %title%")"
 [[ $current_song ]] && empty='  '
 
+list_songs() {
+	mpc playlist | awk '
+		BEGIN { print "'"$empty"'options\n'"$empty"'━━━━━━━" }
+		{ p = ($0 == "'"$current_song"'") ? "'$indicator' " : "'"$empty"'"; print p $0 }'
+}
+
+set_options() {
+	sed -i "/^options/ s/\w*$/$1/" $0
+	options=$1
+}
+
+options=
+
 if [[ -z $@ ]]; then
-	#mpc playlist | awk '$0 == "'"$current_song"'" { $0 = "'$indicator' " $0 } { print }'
-	mpc playlist | awk '{ p = ($0 == "'"$current_song"'") ? "'$indicator' " : "'"$empty"'"; print p $0 }'
+	list_songs
 else
-	song="$@"
-	~/.orw/scripts/play_song_from_playlist.sh "${song:${#empty}}"
-	#~/.orw/scripts/play_song_from_playlist.sh "${@#$indicator }"
+	case $@ in
+		"${empty}options")
+			set_options true
+			sed -i '/^options/ s/\w*$/true/' $0
+			echo -e 'load\nsave\nclear\nrefresh';;
+		refresh) set_options;;
+		clear|save*)
+			set_options
+
+			mpc -q $@
+			action="${@%% *}ed"
+			[[ $@ =~ ' ' ]] && playlist="${@#* }";;
+		load) mpc lsplaylists | grep -v .*.m3u;;
+		*)
+			if [[ $options ]]; then
+				set_options
+				playlist="$@"
+				action="loaded"
+				mpc load "$playlist" > /dev/null
+			else
+				item="$@"
+				~/.orw/scripts/play_song_from_playlist.sh "${item:${#empty}}"
+			fi;;
+	esac
+
+	[[ $@ != load ]] && ~/.orw/scripts/notify.sh -p "<b>$playlist</b> playlist ${action//ee/e}."
+	[[ $options ]] || list_songs
 fi
+
+#current_song="$(mpc current -f "%artist% - %title%")"
+#[[ $current_song ]] && empty='  '
+#
+#if [[ -z $@ || $@ == refresh ]]; then
+#	#mpc playlist | awk '$0 == "'"$current_song"'" { $0 = "'$indicator' " $0 } { print }'
+#	echo -e 'refresh\n━━━━━━━'
+#	mpc playlist | awk '{ p = ($0 == "'"$current_song"'") ? "'$indicator' " : "'"$empty"'"; print p $0 }'
+#else
+#	item="$@"
+#	[[ $item != refresh ]] && ~/.orw/scripts/play_song_from_playlist.sh "${item:${#empty}}"
+#	#~/.orw/scripts/play_song_from_playlist.sh "${@#$indicator }"
+#fi
