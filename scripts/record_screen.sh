6a502155e5cbe0ec43381eaf937cdb1fef0a68c5
s0la <sola931@gmail.com>
2021-08-29 15:38:58 +0200

    Add subdirecotry option

--- scripts/record_screen.sh
+++ scripts/record_screen.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
-while getopts :f:d: flag; do
+base_directory=~/Videos
+sub_directory='bar_manager'
+
+while getopts :f:d:D: flag; do
 	((arg_count++))
 
 	case $flag in
 		f) fps=$OPTARG;;
-		d) display=display_$OPTARG;;
+		d)
+			sub_directory=$OPTARG
+			sed -i "/^sub_directory/ s/'.*'/'$sub_directory'/" $0;;
+		D) display=display_$OPTARG;;
 	esac
 done
 
-[[ ! -f ~/.config/orw/config ]] && ~/.orw/scripts/generate_orw_config.sh
+full_path=$base_directory/$sub_directory
+[[ ! -d $full_path ]] && mkdir -p $full_path
+[[ $sub_directory && $# -eq 2 ]] && exit
 
-#read resolution position <<< $(awk '\
-#	/^'${display:-full_resolution}'/ {
-#		if(/^full/ || xy) print $2 "x" $3, xy
-#		else xy = "+" $2 "," $3 }' ~/.config/orw/config)
+[[ ! -f ~/.config/orw/config ]] && ~/.orw/scripts/generate_orw_config.sh
 
 read resolution position <<< $(awk '\
 	$1 == "primary" { d = ("'$display'") ? "'$display'" : $NF }
@@ -25,11 +30,7 @@ read resolution position <<< $(awk '\
 shift $((arg_count * 2))
 [[ $@ ]] && filename="$@" || filename=$(date +"%Y-%m-%d-%H:%M")
 
-#~/.orw/scripts/notify.sh -p "<span font='Roboto Mono 10'>      </span>recording started"
-#~/.orw/scripts/notify.sh -s osd  -i   'recording started'
-~/.orw/scripts/notify.sh -s osd  -i   'recording started'
-#~/.orw/scripts/notify.sh osd    'recording started'
-#~/.orw/scripts/notify.sh osd     'recording started'
+~/.orw/scripts/notify.sh -s osd  -i   'recording started' &
 
-ffmpeg -y -f x11grab -r ${fps-25} -s $resolution -draw_mouse 1 -i $DISPLAY$position \
-	-f pulse -async 1 -i default -c:v libx264 -preset ultrafast -vsync 1 ~/Videos/$filename.mp4
+ffmpeg -y -f x11grab -r ${fps-20} -s $resolution -draw_mouse 1 -i $DISPLAY$position \
+	-f pulse -async 1 -i default -c:v libx264 -preset ultrafast -vsync 1 $full_path/$filename.mp4
