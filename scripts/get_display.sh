2efeb479d1e5f11ab3de848cafc43cc76052d172
s0la <sola931@gmail.com>
2023-06-20 17:42:16 +0200

    Add offset

--- scripts/get_display.sh
+++ scripts/get_display.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-awk -F '[_ ]' '{ if(/^orientation/) {
+awk -F '[_ ]' '{
+	if (/^orientation/) {
 		cd = 1
 		bmin = 0
 		i = '$1'; mi = i + 2
@@ -8,7 +9,7 @@ awk -F '[_ ]' '{ if(/^orientation/) {
 		wx = '$1'
 		wy = '$2'
 
-		if($NF ~ /^h/) {
+		if ($NF ~ /^h/) {
 			i = 4
 			p = wx
 		} else {
@@ -16,21 +17,21 @@ awk -F '[_ ]' '{ if(/^orientation/) {
 			p = wy
 		}
 	} {
-		if($1 == "display") {
-			if($3 == "xy") {
+		if ($1 == "display") {
+			if ($3 == "xy") {
 				cd = $2
 
 				dx = $4
 				dy = $5
 				minp = $(mi + 1)
-			} else if($3 == "size") {
+			} else if ($3 == "size") {
 				dw = $4
 				dh = $5
 				maxp = minp + $(mi + 1)
 
 				max += $i
 
-				if(p < max) {
+				if (p < max) {
 					print cd, dx, dy, dw, dh, minp, maxp, bmin, bmin + dw, dx + wx, dy + wy
 					exit
 				} else {
@@ -38,6 +39,8 @@ awk -F '[_ ]' '{ if(/^orientation/) {
 					if(p > max) if(i == 4) wx -= $i
 					else wy -= $i
 				}
+			} else if ($3 == "offset") {
+				print $4, $5
 			}
 		}
 	}
