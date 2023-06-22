4d5f7102e6eab5e4bb6482bb5a150e9d734eb196
s0la <sola931@gmail.com>
2023-06-22 20:57:21 +0200

    Add new scripts

---
+++ scripts/rofi_scripts/dmenu/opacity.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+icon_x_down=
+icon_x_up=
+icon_y_up=
+icon_y_down=
+
+while
+	index=$(echo -e '\n' | rofi -dmenu -format i -selected-row $index -theme main)
+	((index)) &&
+		direction=- || direction=+
+	[[ $index ]]
+do
+	~/.orw/scripts/opacityctl.sh term ${direction}4
+done
