3fc3a491fa0ba37ed89eb764b20282c87876ef25
s0la <sola931@gmail.com>
2023-06-22 20:55:42 +0200

    Group into switch menu

--- scripts/rofi_scripts/dmenu/workspaces_group.sh
+++ scripts/rofi_scripts/dmenu/workspaces_group.sh
@@ -1,8 +1,46 @@
 #!/bin/bash
 
+get_workspaces() {
+	awk '
+		/<\/?names>/ { wn = (wn + 1) % 2 }
+
+		wn && /<name>/ {
+			gsub("\\s*<[^>]*>", "")
+			printf "[%s]=%d ", $0, wsc++
+		}' ~/.config/openbox/rc.xml
+}
+
+declare -A workspaces
+eval workspaces=( $(get_workspaces) )
+
+if [[ -z $@ ]]; then
+	rofi -format 'i' -modi ":$0 switch,:$0 wall,:$0 move,:$0 tiling" -show  -theme sidebar_new
+else
+	case $# in
+		1)
+			for ws in ${!workspaces[*]}; do
+				echo ${workspaces[$ws]}, $ws
+			done | sort -nk 1,1 | cut -d ' ' -f 2
+			;;
+		*)
+			read action workspace <<< "$@"
+
+			if [[ $action == move ]]; then
+				id=$(xdotool getactivewindow)
+				~/.orw/scripts/signal_windows_event.sh mv
+				wmctrl -ir $id -t ${workspaces[$workspace]}
+			fi
+
+			wmctrl -s ${workspaces[$workspace]}
+			[[ $action == wall ]] && ~/.orw/scripts/wallctl.sh -r &
+	esac
+fi
+
+exit
+
 path=~/.orw/scripts/rofi_scripts
-theme=$(awk -F '"' 'END { print $(NF - 1) }' ~/.config/rofi/main.rasi)
-[[ $theme =~ icons|dmenu ]] && ${path%/*}/set_rofi_geometry.sh workspaces
+theme=$(awk -F '[".]' 'END { print $(NF - 2) }' ~/.config/rofi/main.rasi)
+#[[ $theme =~ icons|dmenu ]] && ${path%/*}/set_rofi_geometry.sh workspaces
 #${path%/*}/set_rofi_width.sh workspaces
 #${path%/*}/set_rofi_margins.sh
 
