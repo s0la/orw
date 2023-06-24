7d5d3fe6d4e86004161b150b6ce5626be141e2d1
s0la <sola931@gmail.com>
2023-06-24 18:55:18 +0200

    Fix toolbar hover button and scrollbar border indicator

--- dotfiles/.config/Thunar/accels.scm
+++ dotfiles/.config/Thunar/accels.scm
@@ -1,4 +1,4 @@
-; thunar GtkAccelMap rc-file         -*- scheme -*-
+; Thunar GtkAccelMap rc-file         -*- scheme -*-
 ; this file is an automated accelerator map dump
 ;
 ; (gtk_accel_path "<Actions>/ThunarActionManager/restore" "")
