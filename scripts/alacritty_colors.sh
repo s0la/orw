36894402c85d3035e035bcd1c18bf121fbe5d650
s0la <sola931@gmail.com>
2024-05-31 20:36:28 +0200

    Adapt for toml configuration

--- scripts/alacritty_colors.sh
+++ scripts/alacritty_colors.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 awk -i inplace '{
+		if (NR == FNR && NR > 16 && NR < 256) {
+			ac = ac "\n\n[[colors.indexed_colors]]\ncolor = " $NF "\nindex = " NR - 1
+		} else if (/indexed_color/) {
+			print substr(ac, 4)
+			exit
+		}
+
+		{ print }
+	}' ~/.orw/dotfiles/.config/{orw/colorschemes/colors,alacritty/alacritty.toml}
+exit
+
+awk -i inplace '{
 		if (NR == FNR) {
 			if (NR > 16 && NR < 256) {
 				ac = ac ",\n{ index: " NR - 1 ", color: '\''" $NF "'\'' }"
@@ -22,4 +34,4 @@ awk -i inplace '{
 
 			if (/indexed_colors/) ic = 1
 		}
-	}' ~/.config/{orw/colorschemes/colors,alacritty/alacritty.yml}
+	}' ~/.orw/dotfiles/.config/{orw/colorschemes/colors,alacritty/alacritty.yml}
