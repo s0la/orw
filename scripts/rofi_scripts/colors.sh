c23a0b5139affb1790263af0c5a5b8de328010dd
Sasa Kanjuh <sola931@gmail.com>
2024-09-12 18:03:23 +0200

    Reimplement to use image preview script

--- scripts/rofi_scripts/colors.sh
+++ scripts/rofi_scripts/colors.sh
@@ -2,6 +2,23 @@
 
 ocs_root=$HOME/.config/orw/colorschemes
 
+vim_colors=$(awk '
+	$2 ~ "g:[svc]?[bf]g" { gsub("'\''", "", $NF); c = c "\\n.*" $NF }
+	END { print substr(c, 3) }' ~/.config/nvim/colors/orw.vim)
+
+ocs_root=~/.config/orw/colorschemes
+current_ocs=$(grep -zlP "#vim\n$vim_colors" $ocs_root/*.ocs)
+ISF=$'\n' read -d '' active colorschemes <<< $(ls $ocs_root/previews/* |
+	awk '/'"${current_ocs##*/}"'.png/ { a = NR - 1 } { ac = ac "\n" $0 } END { print a ac }')
+
+(
+	echo '~/.orw/scripts/rice_and_shine.sh -tC "$(sed "s/.ocs.png//" <<< "${element##*/}")"'
+	ls $ocs_root/previews/* | awk '
+		/'"${current_ocs##*/}"'.png/ { a = NR - 1 } { ac = ac "\n" $0 }
+		END { print a ac }'
+) | ${0%/*}/dmenu.sh image_preview
+exit
+
 while read preview; do
 	echo -en "${preview##*/}\x00icon\x1f${preview}\n"
 done <<< $(ls $ocs_root/previews/*) |
--- scripts/rofi_scripts/select_wallpaper.sh
+++ scripts/rofi_scripts/select_wallpaper.sh
@@ -11,16 +11,44 @@ get_directory
 
 if [[ -z $@ ]]; then
 	current_desktop=$(xdotool get_desktop)
-	current_wallpaper=$(grep "^desktop_$current_desktop" $config | cut -d '"' -f 2)
+	current_wallpapers="$(awk -F '"' '
+		$1 ~ "^desktop_'$current_desktop'" {
+			for (i=2; i<NF; i++) if (!(i % 2)) cw = cw "|" $i
+			print substr(cw, 2)
+		}' $config)"
+
+	#current_wallpapers="$(awk -F '"' '
+	#	$1 ~ "^desktop_'$current_desktop'" {
+	#		for (i=2; i<NF; i++) {
+	#			if (!(i % 2)) {
+	#				sub(".*/", "", $i)
+	#				sw = sw "|" $i
+	#			}
+	#		}
+	#		print substr(sw, 2)
+	#	}' $config)"
 
 	((depth)) && maxdepth="-maxdepth $depth"
 
-	eval find $directory/ "$maxdepth" -type f -iregex "'.*\(jpe?g\|png\)'" |
-		sort -t '/' -k 1 | awk '{
-						if (/'"${current_wallpaper##*/}"'$/) i = NR - 1
-						sub("'"${root//\'}"'/?", "")
-						print $0
-					} END { printf "\0active\x1f%d\n", i }'
+	##works with rofi sidebar theme (Alt + Shift + s)
+	#eval find $directory/ "$maxdepth" -type f -iregex "'.*\(jpe?g\|png\)'" |
+	#	sort -t '/' -k 1 | awk '{
+	#					#if ("('"$current_wallpapers"')$") i = i "," NR - 1
+	#					if ($0 ~ "('"${current_wallpapers//\|/\\\\\\\\|}"')$") i = i "," NR - 1
+	#					sub("'"${root//\'}"'/?", "")
+	#					print $0
+	#				} END { printf "\0active\x1f%s\n", substr(i, 2) }'
+
+	#works with rofi image preview
+	(
+		echo '~/.orw/scripts/wallctl.sh -s "$element"'
+		eval find $directory/ "$maxdepth" -type f -iregex "'.*\(jpe?g\|png\)'" |
+			sort -t '/' -k 1 | awk '{
+					if ($0 ~ "('"${current_wallpapers//\|/\\\\\\\\|}"')$") i = i "," NR - 1
+					aw = aw "\n" $0
+				} END { print substr(i, 2) aw }'
+				#} END { print substr(i, 2) aw }' | ${0%/*}/image_preview.sh
+	) | ${0%/*}/dmenu.sh image_preview
 else
 	wall="$@"
 	eval ~/.orw/scripts/wallctl.sh -s "$root/'$wall'"
