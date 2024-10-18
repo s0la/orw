2f5820cf4d1a51989f83510aa38bcfcf2d83af7c
Sasa Kanjuh <sola931@gmail.com>
2024-10-18 21:07:37 +0200

    Implement orientation based display ordering

--- scripts/bar/modules/display.sh
+++ scripts/bar/modules/display.sh
@@ -1,29 +1,70 @@
 #!/bin/bash
 
 get_display() {
-	read {{prev,next}_,}display display_count <<< $(awk '
-		NR == FNR && /class="\*"/ { c = 1 }
-		c && /<[xy]>/ {
-			v = $1
-			gsub("\\s*<[^>]*>", "", v)
-			if ($1 ~ "x") x = int(v); else y = int(v)
-		}
-		c && /\/position/ { c = 0 }
+	#read {{prev,next}_,}display display_count <<< $(awk '
+	#	NR == FNR && /class="\*"/ { c = 1 }
+	#	c && /<[xy]>/ {
+	#		v = $1
+	#		gsub("\\s*<[^>]*>", "", v)
+	#		if ($1 ~ "x") x = int(v); else y = int(v)
+	#	}
+	#	c && /\/position/ { c = 0 }
 
-		NR > FNR {
-			if (/[xy]_offset/) { if (/^x/) xo = $NF; else yo = $NF }
-			if (/^display.*(xy|size)/) {
-				if (/xy/) { dx = $2; dy = $3 }
-				else {
-					d = $1
-					gsub("[^0-9]", "", d)
+	#	NR > FNR {
+	#		if (/[xy]_offset/) { if (/^x/) xo = $NF; else yo = $NF }
+	#		if (/^display.*(xy|size)/) {
+	#			if (/xy/) { dx = $2; dy = $3 }
+	#			else {
+	#				d = $1
+	#				gsub("[^0-9]", "", d)
 
-					if (x >= dx + xo && x <= dx + $2 - xo &&
-						y >= dy + yo && y <= dy + $3 - yo) cd = d - 1
-				}
+	#				if (x >= dx + xo && x <= dx + $2 - xo &&
+	#					y >= dy + yo && y <= dy + $3 - yo) cd = d - 1
+	#			}
+	#		}
+	#	} END { print (cd + d - 1) % d + 1, (cd + 1) % d + 1, cd + 1, d }' \
+	#		~/.config/{openbox/rc.xml,orw/config})
+
+
+
+	read {{prev,next}_,}display display_count <<< \
+		$(awk -F '[ x+]' '
+			NR == 1 {
+				h = $9
+				v = $12
+				sub("[^0-9]", "", v)
+				si = (h > 2 * v) ? 2 : 3
 			}
-		} END { print (cd + d - 1) % d + 1, (cd + 1) % d + 1, cd + 1, d }' \
-			~/.config/{openbox/rc.xml,orw/config})
+
+			NR == FNR && $2 == "connected" {
+				ad[$(3 + ($3 == "primary") + si)] = ++d
+			}
+
+			NR > FNR && /class="\*"/ { c = 1 }
+			c && /<monitor>/ {
+				gsub("[^0-9]", "")
+				for (di in ad) {
+					if (++dc == $0) d = ad[di]
+				}
+				exit
+			}
+			END { print (d + dc - 1) % d + 1, (d + 1) % dc + 1, d, dc }' \
+				<(xrandr) ~/.config/openbox/rc.xml)
+
+
+
+
+
+	#read {prev,next,current}_display display_count <<< $(awk '
+	#	NR == FNR && /class="\*"/ { c = 1 }
+	#	c && /<monitor>/ {
+	#		c = 0
+	#		d = $1
+	#		gsub("\\s*<[^>]*>", "", v)
+	#	}
+	#	NR > FNR && /display.*name/ { dc++ }
+	#	END { print (d + dc - 1) % dc + 1, (d + 1) % dc + 1, cd + 1, dc }' \
+	#		~/.config/{openbox/rc.xml,orw/config})
 
 	[[ $display_icons == only ]] &&
 		icon=$(get_icon "number_$display") ||
@@ -43,6 +84,23 @@ set_display_actions() {
 
 make_display_content() {
 	display_icons=$icons
+	display_mapping=$(
+		xrandr | awk -F '[ x+]' '
+			NR == 1 {
+				h = $9
+				v = $12
+				sub("[^0-9]", "", v)
+				si = (h > 2 * v) ? 2 : 3
+			}
+			$2 == "connected" {
+				ad[$(3 + ($3 == "primary") + si)] = ++d
+			} END {
+				for (d in ad) printf "[%d]=%d ", ++di, ad[d]
+			}'
+	)
+
+	declare -A display_map
+	eval display_map=( $display_mapping )
 }
 
 check_display() {
@@ -62,6 +120,6 @@ check_display() {
 				set_display_actions
 				((display_count > 1)) && print_module display
 			fi
-		done < <(inotifywait -me close_write --format '%e %w%f' ~/.config/openbox 2> /dev/null)
+		done < <(inotifywait -me close_write --format '%e %w%f' ~/.config/openbox/ 2> /dev/null)
 	fi
 }
