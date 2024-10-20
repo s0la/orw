dc6afe5c160ece70c25ce74ff1968610e60f223b
Sasa Kanjuh <sola931@gmail.com>
2024-10-20 17:39:16 +0200

    Change formatting

--- scripts/song_notification.sh
+++ scripts/song_notification.sh
@@ -4,11 +4,21 @@ artist=$(mpc current -f %artist%)
 title=$(mpc current -f %title%)
 album=$(mpc current -f %album%)
 
-cover=~/Music/covers/${album// /_}.jpg
+#cover=~/Music/covers/${album// /_}.jpg
+cover="$(~/.orw/scripts/get_cover_art.sh)"
 cover="${cover//[()]/}"
 
+#song_position=$(mpc current -f '%position%')
+#IFS=$'\n' read -d '' -a songs <<< $(mpc playlist |
+#	awk 'NR >= '$song_position' - 1 && NR <= '$song_position' + 1')
+
 font='Iosevka Orw'
-info="$artist  <span font='$font 9'></span>  <b>$title</b>"
+#info+="<span font='$font 8'><b>$artist┃</b>$title</span>"
+info="<b>$artist┃</b>$title"
+#song_fg="<span foreground='\$sbg'>"
+#[[ ${songs[-3]} ]] && info="$song_fg${songs[-3]}</span>\n"
+#info+="<span foreground='\$fg'>$artist  <span font='$font 9'></span>  <b>$title</b></span>\n"
+#[[ ${songs[2]} ]] && info+="$song_fg${songs[2]}</span>"
 
 [[ -f $cover ]] && icon="-i $cover"
 [[ $artist && $title ]] &&
