3e2297c6793887c6f2f6c9a974b9819024977fc4
Sasa Kanjuh <sola931@gmail.com>
2024-10-20 17:33:54 +0200

    Add bluetooth notifications and mini theme

--- scripts/system_notification.sh
+++ scripts/system_notification.sh
@@ -1,35 +1,62 @@
 #!/bin/bash
 
 theme=default
+replace_id=102
 [[ $1 =~ mpd ]] && theme=${2:-vertical}
 
-[[ $1 == brightness ]] &&
-	command='echo 50%' replace_id=104 theme=osd ||
-	command=$([[ $1 =~ mpd ]] && echo mpc volume || echo amixer -D pulse get Master) replace_id=102
+if [[ $1 == bluetooth ]]; then
+	theme=osd
+	[[ $2 ]] &&
+		state=$2 || state="$(bluetoothctl show | sed -n 's/.*PowerState:\s*//p')"
+	value="BLUETOOTH: $state"
+	icon=$(sed -n 's/bluetooth=//p' ~/.orw/scripts/icons)
+else
+	case $1 in
+		mpd*) command='mpc volume';;
+		brightness)
+			theme=osd
+			command="echo ' $2%'";;
+		*)
+			theme=mini
+			command='amixer -D pulse get Master'
+			;;
+	esac
 
-read value level_value empty_value icon <<< $($command | \
-	awk '\
-		BEGIN { s = "'$theme'" }
-			("'$1'" ~ "system" && /^ *Front/) || "'$1'" !~ "system" {
-			v = gensub(/.*[ \[]([0-9]+)%.*/, "\\1", 1)
-			m = (!v || $NF ~ "off")
+	read value level_value empty_value icon <<< $(awk '
+				BEGIN { s = "'$theme'" }
 
-			if(s == "vertical") st = 10
-			else st = ("'$theme'" == "default") ? 6 : 5
-			t = int(100 / st)
-			l = int(v / st)
+				("'$1'" ~ "system" && /^ *Front/) || "'$1'" !~ "system" {
+					v = gensub(/.*[ \[]([0-9]+)%.*/, "\\1", 1)
+					m = (!v || $NF ~ "off")
 
-			b = "'$1'" == "brightness"
-			if(!v || $NF ~ "off") i = ""
-			else if(v < 35) i = (b) ? "" : ""
-			else if(v < 65) i = (b) ? "" : ""
-			else i = (b) ? "" : ""
-			if(b) i = ""
+					#if(s == "vertical") st = 10
+					#else st = ("'$theme'" == "default") ? 10 : 5
 
-			printf "%d %.0f %.0f %s", v, l, t - l, i
-			exit
-		}')
+					switch ("'"$theme"'") {
+						case "vertical": st = 10; break
+						case "mini": st = 10; break
+						case "osd": st = 5; break
+						default: st = 6
+					}
 
-[[ $1 =~ mpd ]] && icon= replace_id=103
-[[ $theme == mini ]] && value="-v $value%" || bar="-b $level_value/$empty_value"
-~/.orw/scripts/notify.sh -r $replace_id -s $theme -t 2200m -i $icon "${bar:-$value}" &> /dev/null &
+					t = int(100 / st)
+					l = int(v / st)
+
+					b = "'$1'" == "brightness"
+					if(!v || $NF ~ "off") i = ""
+					else if(v < 35) i = (b) ? "" : ""
+					else if(v < 65) i = (b) ? "" : ""
+					else i = (b) ? "" : ""
+
+					printf "%d %.0f %.0f %s", v, l, t - l, i
+					exit
+				}' <<< $($command))
+
+	[[ $1 =~ mpd ]] && icon= replace_id=103
+	#[[ $theme == mini ]] && value="-v $value%" || bar="-b $level_value/$empty_value"
+	[[ $theme == mini ]] && value="-v $value%"
+	bar="-b $level_value/$empty_value"
+fi
+
+~/.orw/scripts/notify.sh -r $replace_id -s $theme -t 2200m -i $icon "$bar" "$value" &> /dev/null &
+#echo ~/.orw/scripts/notify.sh -r $replace_id -s $theme -t 2200m -i $icon "${bar:-$value}" #&> /dev/null &
