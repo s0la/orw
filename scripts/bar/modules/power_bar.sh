d2369599684f32e5fe4f311f7c74fd64fbecc450
Sasa Kanjuh <sola931@gmail.com>
2024-11-13 17:21:56 +0100

    Change power bar frame and font size

--- scripts/bar/modules/power_bar.sh
+++ scripts/bar/modules/power_bar.sh
@@ -1,18 +1,18 @@
 pid='$(ps -C lemonbar -o pid= --sort=-start_time | head -1)'
 
-separator='48'
-actions+="%{O15}%{A:kill $pid && ~/.orw/scripts/lock_screen.sh:}%{I}%{I}%{A}%{O15}"
-actions+="%{O15}%{A:kill $pid :}%{I}%{I}%{A}%{O15}"
-actions+="%{O15}%{A:kill $pid && openbox --exit:}%{I}%{I}%{A}%{O15}"
-actions+="%{O15}%{A:kill $pid :}%{I}%{I}%{A}%{O15}"
-actions+="%{O15}%{A:kill $pid :}%{I}%{I}%{A}%{O15}"
+separator='38'
+actions+="%{O12}%{A:kill $pid && ~/.orw/scripts/lock_screen.sh:}$fg%{T3}%{T-}%{A}%{O12}"
+actions+="%{O12}%{A:kill $pid && sudo systemctl reboot:}$fg%{T3}%{T-}%{A}%{O12}"
+actions+="%{O12}%{A:kill $pid && openbox --exit:}$fg%{T3}%{T-}%{A}%{O12}"
+actions+="%{O12}%{A:kill $pid && sudo systemctl poweroff:}$fg%{T3}%{T-}%{A}%{O12}"
+actions+="%{O12}%{A:kill $pid && sudo systemctl close:}%{F#63a585}%{T3}%{T-}%{A}%{O12}"
 
-bg='#161a1b'
-fg='#80d4d9'
-fc='#161a1b'
-font='material:size=17'
-geometry='480x162+720+459'
+bg='#dd151c20'
+fg='#323e45'
+fc='#2c383e'
+font='material:size=13'
+geometry='384x162+768+459'
 
 echo -e "%{c}$actions" | lemonbar \
--p -d -B$bg -F$fg -R$fc -r 3 \
+-p -d -B$bg -F$fg -R$fc -r 2 \
 -f "$font" -g $geometry -n power_bar | bash
