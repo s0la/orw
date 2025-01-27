ba0024f77667aa30db3b06a2a36e2d00ed57eced
Sasa Kanjuh <sola931@gmail.com>
2025-01-27 01:30:48 +0100

    Refactor to stay on the same workspace

--- scripts/print_borders.sh
+++ scripts/print_borders.sh
@@ -2,16 +2,39 @@
 
 [[ $1 ]] || id=$(wmctrl -l | awk '$2 >= 0 { print $1; exit }')
 
-current_workspace=$(xdotool get_desktop)
+toggle_decor_state() {
+	awk -F '[<>]' '
+			/class=.*custom_size/ { aw = 1 }
 
-tiling_workspaces=$(sed -n '/^tiling/ s/.*(\s*\|\s*)//gp' \
-	~/.orw/scripts/spy_windows.sh | tr ' ' '\n')
+			aw && /<decor>/ {
+				cds = $3
+				nds = ("'"$1"'") ? "'"$1"'" : ($3 == "no") ? "yes" : "no"
+				sub($3, nds)
+				aw = 0
+			}
 
-floating_workspace=$(comm -3 \
-	<(wmctrl -d | cut -d ' ' -f 1) \
-	<(echo -e "$tiling_workspaces") | head)
+			{ wo = wo "\n" $0 }
 
-wmctrl -s $floating_workspace
+			END {
+				print cds wo
+			}' ~/.config/openbox/rc.xml |
+				{ read cd; { echo $cd >&1; cat > ~/.config/openbox/rc.xml; } }
+
+	openbox --reconfigure
+}
+
+current_decor_state=$(toggle_decor_state yes)
+
+#current_workspace=$(xdotool get_desktop)
+#
+#tiling_workspaces=$(sed -n '/^tiling/ s/.*(\s*\|\s*)//gp' \
+#	~/.orw/scripts/spy_windows.sh | tr ' ' '\n')
+#
+#floating_workspace=$(comm -3 \
+#	<(wmctrl -d | cut -d ' ' -f 1) \
+#	<(echo -e "$tiling_workspaces") | head -1)
+#
+#wmctrl -s $floating_workspace
 
 fifo=/tmp/borders.fifo
 mkfifo $fifo
@@ -23,5 +46,7 @@ alacritty -t get_borders --class=custom_size -e \
 read x_border y_border < $fifo
 rm $fifo
 
-wmctrl -s $current_workspace
+#wmctrl -s $current_workspace
 echo $x_border $y_border
+
+toggle_decor_state ${current_decor_state} &> /dev/null
