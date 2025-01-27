cac75ddaa4d6a1624c57f6e2a30fab7e02403f74
Sasa Kanjuh <sola931@gmail.com>
2025-01-27 01:37:24 +0100

    Minor formatting adjustments

--- scripts/song_notification.sh
+++ scripts/song_notification.sh
@@ -15,6 +15,7 @@ cover="${cover//[()]/}"
 font='Iosevka Orw'
 #info+="<span font='$font 8'><b>$artist┃</b>$title</span>"
 info="<b>$artist┃</b>$title"
+info="<b>$artist</b>\n$title"
 #song_fg="<span foreground='\$sbg'>"
 #[[ ${songs[-3]} ]] && info="$song_fg${songs[-3]}</span>\n"
 #info+="<span foreground='\$fg'>$artist  <span font='$font 9'></span>  <b>$title</b></span>\n"
@@ -22,4 +23,5 @@ info="<b>$artist┃</b>$title"
 
 [[ -f $cover ]] && icon="-i $cover"
 [[ $artist && $title ]] &&
-	~/.orw/scripts/notify.sh $icon -r 101 -F 'Iosevka Orw' -f 8 -po 8 "$info" &> /dev/null
+	#~/.orw/scripts/notify.sh $icon -r 101 -F 'Iosevka Orw' -f 8 -po 8 "$info" &> /dev/null
+	~/.orw/scripts/notify.sh $icon -r 101 -F 'Iosevka Orw' -f 8 -o 8 "$info" &> /dev/null
