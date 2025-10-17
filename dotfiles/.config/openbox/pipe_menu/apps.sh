c14fffa02868c3584ddb2eb95524fc4a6f305e61
Sasa Kanjuh <sola931@gmail.com>
2025-10-17 18:27:47 +0200

    Replace termite for alacritty

--- dotfiles/.config/openbox/pipe_menu/apps.sh
+++ dotfiles/.config/openbox/pipe_menu/apps.sh
@@ -3,4 +3,4 @@
 ~/.config/openbox/pipe_menu/generate_menu.sh \
     -m '<menu id="ncmpcpp"/>' \
     -i 'tile ~/.orw/scripts/tile_terminal_mouse.sh' \
-	'terminal:termite' 'menu' 'dropdown:~/.orw/scripts/dropdown.sh' 'file_manager:thunar' 'web_browser:firefox'
+	'terminal:alacritty' 'menu' 'dropdown:~/.orw/scripts/dropdown.sh' 'file_manager:thunar' 'web_browser:firefox'
