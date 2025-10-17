82c586ae28eadafe426dace21dff7157a55960a1
Sasa Kanjuh <sola931@gmail.com>
2025-10-17 18:55:31 +0200

    Handle menu toggle from within spy_windows

--- scripts/rofi_scripts/menu_template.sh
+++ scripts/rofi_scripts/menu_template.sh
@@ -5,6 +5,6 @@ items=( $(sed -n "s/$icons_template//p" $icons) )
 item_count=${#items[*]}
 set_theme_str
 
-toggle
+#toggle
 
 tr ' ' '\n' <<< ${items[*]} | rofi -dmenu -format i -theme-str "$theme_str" -theme main
