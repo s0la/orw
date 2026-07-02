154f5a9381f73c3475450b379b020613a7604395
Sasa Kanjuh <sola931@gmail.com>
2026-07-02 18:36:38 +0200

    Add update interval, so it refreshes info while it's visible

--- scripts/show_torrents_info_old.sh
+++ scripts/show_torrents_info_old.sh
@@ -8,40 +8,46 @@ pd_span="<span foreground='\\\$pbfg'>"
 pr_span="<span foreground='\\\$sbg'>"
 end_span='</span></span></span>'
 
-torrent_info=$(transmission-remote -l | awk '
-	function print_progress(percent) {
-		#if(percent > 0) return gensub(/ /, "▀", "g", sprintf("%*s", percent, " "))
-		if(percent > 0) return gensub(/ /, "━", "g", sprintf("%*s", percent, " "))
-	}
-
-	NR == 1 {
-		ns = index($0, "Name")
-		ss = index($0, "Status")
-		us = index($0, "Up"); ds = index($0, "Down")
-	} {
-		ts = substr($0, ss, ns - ss)
-		if ($2 ~ "^[0-9]{1,2}(\\.[0-9]{1,2})?%") {
-			tc++
-			tn = substr($0, ns)
-			tnl = length(tn)
-			if(tnl > mtnl) mtnl = tnl
-			s = 7
-			pd = sprintf("%.0f", $2 / s)
-			pr = 100 / s - pd
-			at[tc,1] = tn
-
-			if($3 != "None") { at[tc,2] = $3 " " $4 "    " substr($0, us + 2, ds - us + 2) }
-			at[tc,3] = "'"$pd_span"'" print_progress(pd)
-			at[tc,4] = "'"$pr_span"'" print_progress(pr) "'$end_span'"
+get_torrent_info() {
+	torrent_info=$(transmission-remote -l | awk '
+		function print_progress(percent) {
+			#if(percent > 0) return gensub(/ /, "▀", "g", sprintf("%*s", percent, " "))
+			if(percent > 0) return gensub(/ /, "━", "g", sprintf("%*s", percent, " "))
 		}
-	} END {
-		for(i = 1; i <= tc; i++) {
-			ft = ft sprintf("%-5s%s%-10s   %s %-*s     %20s", "'"$icon"'", \
-				at[i, 3], at[i, 4], "'"$tn_span"'", mtnl, at[i, 1], at[i, 2]) #, at[i, 3], at[i, 4])
-			if(i < tc) ft = ft "\\\\n\\\\n"
-		}
+
+		NR == 1 {
+			ns = index($0, "Name")
+			ss = index($0, "Status")
+			us = index($0, "Up"); ds = index($0, "Down")
+		} {
+			ts = substr($0, ss, ns - ss)
+			if ($2 ~ "^[0-9]{1,2}(\\.[0-9]{1,2})?%") {
+				tc++
+				tn = substr($0, ns)
+				tnl = length(tn)
+				if(tnl > mtnl) mtnl = tnl
+				s = 7
+				pd = sprintf("%.0f", $2 / s)
+				pr = 100 / s - pd
+				at[tc,1] = tn
+
+				if($3 != "None") { at[tc,2] = $3 " " $4 "    " substr($0, us + 2, ds - us + 2) }
+				at[tc,3] = "'"$pd_span"'" print_progress(pd)
+				at[tc,4] = "'"$pr_span"'" print_progress(pr) "'$end_span'"
+			}
+		} END {
+			for(i = 1; i <= tc; i++) {
+				ft = ft sprintf("%-5s%s%-10s   %s %-*s     %20s", "'"$icon"'", \
+					at[i, 3], at[i, 4], "'"$tn_span"'", mtnl, at[i, 1], at[i, 2]) #, at[i, 3], at[i, 4])
+				if(i < tc) ft = ft "\\\\n\\\\n"
+			}
 
 		print ft
 	}' 2> /dev/null)
+}
 
-~/.orw/scripts/notify.sh -t 5 -pP 3 -o 3 "$torrent_info"
+for i in {1..5}; do
+	get_torrent_info
+	~/.orw/scripts/notify.sh -t 1100m -r 22 -pP 3 -o 3 "$torrent_info" &
+	sleep 1
+done
