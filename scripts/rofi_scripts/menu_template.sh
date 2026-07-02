6f4444d692f1134698279574b8364b05bdeec3a4
Sasa Kanjuh <sola931@gmail.com>
2026-07-02 18:26:00 +0200

    Add support for different icon templates

--- scripts/rofi_scripts/menu_template.sh
+++ scripts/rofi_scripts/menu_template.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-icons_template="$1"
-items=( $(sed -n "s/$icons_template//p" $icons) )
+[[ "$1" == *= ]] &&
+	icons_template="$1" &&
+	items=( $(sed -n "s/$icons_template//p" $icons) ) ||
+	eval "items=( "$1" )"
+
+[[ $2 ]] && positions="$2"
+
 item_count=${#items[*]}
 set_theme_str
 
 #toggle
 
-tr ' ' '\n' <<< ${items[*]} | rofi -dmenu -format i -theme-str "$theme_str" -theme main
+tr ' ' '\n' <<< ${items[*]} |
+	rofi -dmenu -format i $positions -theme-str "$theme_str" -theme main
