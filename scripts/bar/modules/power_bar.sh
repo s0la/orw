8698e739e355d3cb36868cc5ee8d5df626531651
Sasa Kanjuh <sola931@gmail.com>
2026-07-05 14:30:28 +0200

    Geometry change

--- scripts/bar/modules/power_bar.sh
+++ scripts/bar/modules/power_bar.sh
@@ -1,17 +1,17 @@
 pid='$(ps -C lemonbar -o pid= --sort=-start_time | head -1)'
 
-separator='28'
+separator='38'
 actions+="%{O12}%{A:kill $pid && ~/.orw/scripts/lock_screen.sh:}$fg%{T3}%{T-}%{A}%{O12}"
 actions+="%{O12}%{A:kill $pid && sudo systemctl reboot:}$fg%{T3}%{T-}%{A}%{O12}"
 actions+="%{O12}%{A:kill $pid && openbox --exit:}$fg%{T3}%{T-}%{A}%{O12}"
 actions+="%{O12}%{A:kill $pid && sudo systemctl poweroff:}$fg%{T3}%{T-}%{A}%{O12}"
-actions+="%{O12}%{A:kill $pid && sudo systemctl close:}%{F#b66b75}%{T3}%{T-}%{A}%{O12}"
+actions+="%{O12}%{A:kill $pid && sudo systemctl close:}%{F#b78b8b}%{T3}%{T-}%{A}%{O12}"
 
 bg='#ff232323'
 fg='#4d4d4d'
-fc='#343434'
+fc='#ff232323'
 font='material:size=13'
-geometry='288x135+576+382'
+geometry='384x162+768+459'
 
 echo -e "%{c}$actions" | lemonbar \
 -p -d -B$bg -F$fg -R$fc -r 2 \
